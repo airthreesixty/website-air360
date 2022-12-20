e907100be2275b3425d0d2c927bb977b6bbc4969
Koki Takahashi <kokitakahashi@MAC047.local>
2022-12-20 18:03:07 +0900

    Added the texts

--- windi.config.ts
+++ windi.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'windicss/helpers'
 
 export default defineConfig({
-  plugins: [require('windicss/plugin/typography')],
+  plugins: [require('windicss/plugin/typography'), require('flowbite/plugin')],
+  content: [
+    './node_modules/flowbite/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
 })
