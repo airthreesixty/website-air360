018c20f055f5ad2aa914b95716cb3aa330a4d7ff
Koki Takahashi <kokitakahashi@MAC047.local>
2022-12-21 10:30:05 +0900

    Made the default accordion fold

--- components/Menu.vue
+++ components/Menu.vue
@@ -34,12 +34,12 @@
       </button>
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul
-          class="flex flex-col p-4 mt-4 border items-center border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          class="flex flex-col p-4 mt-4 border items-center border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
           <li>
             <a
               href="#"
-              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-gray-700 md:p-0 dark:text-white"
               aria-current="page"
               >Why Air360</a
             >
--- components/content/ConnectedSection.vue
+++ components/content/ConnectedSection.vue
@@ -8,7 +8,7 @@
             type="button"
             class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
             data-accordion-target="#accordion-open-body-12"
-            aria-expanded="true"
+            aria-expanded="false"
             aria-controls="accordion-open-body-12"
           >
             <span class="flex items-center"
