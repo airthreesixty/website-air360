18d92ac09633c3a945f75600f8e1ce54b93926ab
Koki Takahashi <kokitakahashi@MAC047.local>
2022-12-21 10:32:53 +0900

    Changed whyair360's style

--- components/Menu.vue
+++ components/Menu.vue
@@ -39,7 +39,7 @@
           <li>
             <a
               href="#"
-              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-gray-700 md:p-0 dark:text-white"
+              class="block py-2 pl-3 pr-4 text-gray-700 bg-white md:bg-transparent md:text-gray-700 md:p-0 dark:text-white"
               aria-current="page"
               >Why Air360</a
             >
