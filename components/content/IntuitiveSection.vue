9a9bd2c5df49982c0b9a1f32460d726a84569845
Koki Takahashi <kokitakahashi@MAC047.local>
2022-12-22 13:57:43 +0900

    Added the some translation

--- components/content/IntuitiveSection.vue
+++ components/content/IntuitiveSection.vue
@@ -58,19 +58,10 @@
             class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900"
           >
             <p class="mb-2 text-white dark:text-gray-400">
-              Flowbite is an open-source library of interactive components built
-              on top of Tailwind CSS including buttons, dropdowns, modals,
-              navbars, and more.
+              <ContentSlot :use="$slots.description1" />
             </p>
             <p class="text-white dark:text-gray-400">
-              Check out this guide to learn how to
-              <a
-                href="/docs/getting-started/introduction/"
-                class="text-blue-600 dark:text-blue-500 hover:underline"
-                >get started</a
-              >
-              and start developing websites even faster with components on top
-              of Tailwind CSS.
+              <ContentSlot :use="$slots.description1_2" />
             </p>
           </div>
         </div>
@@ -121,19 +112,10 @@
             class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700"
           >
             <p class="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is first conceptualized and designed using the Figma
-              software so everything you see in the library has a design
-              equivalent in our Figma file.
+              <ContentSlot :use="$slots.description2" />
             </p>
             <p class="text-gray-500 dark:text-gray-400">
-              Check out the
-              <a
-                href="https://flowbite.com/figma/"
-                class="text-blue-600 dark:text-blue-500 hover:underline"
-                >Figma design system</a
-              >
-              based on the utility classes from Tailwind CSS and components from
-              Flowbite.
+              <ContentSlot :use="$slots.description2_2" />
             </p>
           </div>
         </div>
@@ -185,19 +167,10 @@
             class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700"
           >
             <p class="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is first conceptualized and designed using the Figma
-              software so everything you see in the library has a design
-              equivalent in our Figma file.
+              <ContentSlot :use="$slots.description3" />
             </p>
             <p class="text-gray-500 dark:text-gray-400">
-              Check out the
-              <a
-                href="https://flowbite.com/figma/"
-                class="text-blue-600 dark:text-blue-500 hover:underline"
-                >Figma design system</a
-              >
-              based on the utility classes from Tailwind CSS and components from
-              Flowbite.
+              <ContentSlot :use="$slots.description3_1" />
             </p>
           </div>
         </div>
@@ -249,19 +222,10 @@
             class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700"
           >
             <p class="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is first conceptualized and designed using the Figma
-              software so everything you see in the library has a design
-              equivalent in our Figma file.
+              <ContentSlot :use="$slots.description4" />
             </p>
             <p class="text-gray-500 dark:text-gray-400">
-              Check out the
-              <a
-                href="https://flowbite.com/figma/"
-                class="text-blue-600 dark:text-blue-500 hover:underline"
-                >Figma design system</a
-              >
-              based on the utility classes from Tailwind CSS and components from
-              Flowbite.
+              <ContentSlot :use="$slots.description4_2" />
             </p>
           </div>
         </div>
@@ -312,37 +276,11 @@
             class="p-5 font-light border border-t-0 border-gray-200 dark:border-gray-700"
           >
             <p class="mb-2 text-gray-500 dark:text-gray-400">
-              The main difference is that the core components from Flowbite are
-              open source under the MIT license, whereas Tailwind UI is a paid
-              product. Another difference is that Flowbite relies on smaller and
-              standalone components, whereas Tailwind UI offers sections of
-              pages.
+              <ContentSlot :use="$slots.description5" />
             </p>
             <p class="mb-2 text-gray-500 dark:text-gray-400">
-              However, we actually recommend using both Flowbite, Flowbite Pro,
-              and even Tailwind UI as there is no technical reason stopping you
-              from using the best of two worlds.
+              <ContentSlot :use="$slots.description5_2" />
             </p>
-            <p class="mb-2 text-gray-500 dark:text-gray-400">
-              Learn more about these technologies:
-            </p>
-            <ul class="pl-5 text-gray-500 list-disc dark:text-gray-400">
-              <li>
-                <a
-                  href="https://flowbite.com/pro/"
-                  class="text-blue-600 dark:text-blue-500 hover:underline"
-                  >Flowbite Pro</a
-                >
-              </li>
-              <li>
-                <a
-                  href="https://tailwindui.com/"
-                  rel="nofollow"
-                  class="text-blue-600 dark:text-blue-500 hover:underline"
-                  >Tailwind UI</a
-                >
-              </li>
-            </ul>
           </div>
         </div>
       </div>
