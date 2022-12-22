2ed3618a732231327884a0d6ed0e0f67c02faade
Koki Takahashi <kokitakahashi@MAC047.local>
2022-12-22 10:19:51 +0900

    Added the missing border line

--- components/content/IntuitiveSection.vue
+++ components/content/IntuitiveSection.vue
@@ -309,23 +309,40 @@
           aria-labelledby="accordion-open-heading-11"
         >
           <div
-            class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700"
+            class="p-5 font-light border border-t-0 border-gray-200 dark:border-gray-700"
           >
             <p class="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is first conceptualized and designed using the Figma
-              software so everything you see in the library has a design
-              equivalent in our Figma file.
+              The main difference is that the core components from Flowbite are
+              open source under the MIT license, whereas Tailwind UI is a paid
+              product. Another difference is that Flowbite relies on smaller and
+              standalone components, whereas Tailwind UI offers sections of
+              pages.
             </p>
-            <p class="text-gray-500 dark:text-gray-400">
-              Check out the
-              <a
-                href="https://flowbite.com/figma/"
-                class="text-blue-600 dark:text-blue-500 hover:underline"
-                >Figma design system</a
-              >
-              based on the utility classes from Tailwind CSS and components from
-              Flowbite.
+            <p class="mb-2 text-gray-500 dark:text-gray-400">
+              However, we actually recommend using both Flowbite, Flowbite Pro,
+              and even Tailwind UI as there is no technical reason stopping you
+              from using the best of two worlds.
             </p>
+            <p class="mb-2 text-gray-500 dark:text-gray-400">
+              Learn more about these technologies:
+            </p>
+            <ul class="pl-5 text-gray-500 list-disc dark:text-gray-400">
+              <li>
+                <a
+                  href="https://flowbite.com/pro/"
+                  class="text-blue-600 dark:text-blue-500 hover:underline"
+                  >Flowbite Pro</a
+                >
+              </li>
+              <li>
+                <a
+                  href="https://tailwindui.com/"
+                  rel="nofollow"
+                  class="text-blue-600 dark:text-blue-500 hover:underline"
+                  >Tailwind UI</a
+                >
+              </li>
+            </ul>
           </div>
         </div>
       </div>
