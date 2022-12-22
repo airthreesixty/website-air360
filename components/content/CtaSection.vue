ec863bfe2d273b5c332d5f84500b36491cb77a20
koki <koki.takahashi@scalefast.com>
2022-12-22 17:38:19 +0900

    Styled CTA section for tablet

--- components/content/CtaSection.vue
+++ components/content/CtaSection.vue
@@ -2,19 +2,20 @@
   <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div
-        class="gap-16 flex-col justify-between items-center p-6 bg-white rounded-lg dark:bg-gray-800 lg:flex lg:gap-24 md:p-12"
+        class="gap-16 flex-col justify-between items-center p-6 bg-white rounded-lg dark:bg-gray-800 md:p-6 lg:flex lg:gap-24"
       >
-        <div class="w-full text-center">
-          <h2 class="mb-4 text-3xl tracking-tight font-extrabold sm:text-4xl">
-            Need to learn more?
-          </h2>
-        </div>
-        <div class="mt-6 w-full lg:mt-0">
+        <h2
+          class="w-full text-center mb-10 text-3xl tracking-tight font-extrabold sm:text-3xl"
+        >
+          Need to learn more?
+        </h2>
+
+        <div class="my-6 w-full lg:mt-0">
           <div class="items-center mb-3 space-y-4 sm:flex sm:space-y-0">
             <div class="w-full text-center">
               <a
                 href="#"
-                class="text-white bg-[#e74b91] hover:bg-[#ce3077] focus:ring-4 focus:ring-[#ce3077] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                class="text-white bg-[#e74b91] text-lg font-bold hover:bg-[#ce3077] focus:ring-4 focus:ring-[#ce3077] font-medium rounded-lg text-sm px-8 py-4 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 >Request a demo</a
               >
             </div>
