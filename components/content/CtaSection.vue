1c75cea40fe13f33b02d005f43f758e10d8b8083
koki <koki.takahashi@scalefast.com>
2022-12-22 17:58:37 +0900

    Styled CTA section for desktop

--- components/content/CtaSection.vue
+++ components/content/CtaSection.vue
@@ -2,10 +2,10 @@
   <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div
-        class="gap-16 flex-col justify-between items-center p-6 bg-white rounded-lg dark:bg-gray-800 md:p-6 lg:flex lg:gap-24"
+        class="gap-16 flex-col justify-between items-center p-6 bg-white rounded-lg dark:bg-gray-800 md:p-6 lg:flex lg:gap-8"
       >
         <h2
-          class="w-full text-center mb-10 text-3xl tracking-tight font-extrabold sm:text-3xl"
+          class="w-full text-center mb-10 text-3xl tracking-tight font-extrabold lg:text-5xl"
         >
           Need to learn more?
         </h2>
@@ -15,7 +15,7 @@
             <div class="w-full text-center">
               <a
                 href="#"
-                class="text-white bg-[#e74b91] text-lg font-bold hover:bg-[#ce3077] focus:ring-4 focus:ring-[#ce3077] font-medium rounded-lg text-sm px-8 py-4 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                class="text-white bg-[#e74b91] text-lg font-bold hover:bg-[#ce3077] focus:ring-4 focus:ring-[#ce3077] font-medium rounded-lg text-sm px-8 py-4 mr-2 mb-2 lg:text-xl dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 >Request a demo</a
               >
             </div>
