3c84955b313941b7b9a6bab8d76702cbe3b08f2c
koki <koki.takahashi@scalefast.com>
2022-12-26 21:32:03 +0900

    Organized the testimonial section

--- components/content/Features.vue
+++ components/content/Features.vue
@@ -5,11 +5,10 @@
         <h2
           class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
         >
-          Secure platform, secure data
+          Some Title
         </h2>
         <p class="font-light text-gray-500 dark:text-gray-400 sm:text-xl">
-          Here at Flowbite we focus on markets where technology, innovation, and
-          capital can unlock long-term value and drive economic growth.
+          Some Texts
         </p>
       </div>
       <div
