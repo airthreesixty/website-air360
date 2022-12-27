9c844f926de8746a13d08f59ebf3206e9002536d
koki <koki.takahashi@scalefast.com>
2022-12-27 16:55:11 +0900

    Translated CTA section

--- components/content/CtaSection.vue
+++ components/content/CtaSection.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="bg-white mb-5 dark:bg-gray-900">
-    <div class="pt-6 px-4 mx-auto max-w-screen-xl lg:py-10 lg:px-6">
+  <section class="bg-white dark:bg-gray-900">
+    <div class="pt-6 px-4 mx-auto max-w-screen-xl lg:px-6">
       <div
         class="gap-16 flex-col justify-between items-center p-6 bg-white rounded-lg dark:bg-gray-800 md:p-6 lg:flex lg:gap-8"
       >
@@ -13,11 +13,11 @@
         <div class="mt-6 w-full lg:mt-0">
           <div class="items-center mb-3 space-y-4 sm:flex sm:space-y-0">
             <div class="w-full text-center">
-              <a
-                href="#"
+              <button
                 class="text-white bg-[#e74b91] text-base font-bold hover:bg-[#ce3077] focus:ring-4 focus:ring-[#ce3077] font-medium rounded-lg text-sm px-6 py-3 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                >Request a demo</a
               >
+                <ContentSlot :use="$slots.action" />
+              </button>
             </div>
           </div>
         </div>
