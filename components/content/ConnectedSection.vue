09815b9fbe682c04293a5d6fd45de5478274cd3c
koki <koki.takahashi@scalefast.com>
2022-12-28 12:08:07 +0900

    Added the icon color prop

--- components/content/ConnectedSection.vue
+++ components/content/ConnectedSection.vue
@@ -5,196 +5,42 @@
         <h2 class="text-blue-500 text-3xl font-extrabold pb-3">
           <ContentSlot :use="$slots.connected" />
         </h2>
-        <h2 id="accordion-collapse-heading-12">
-          <button
-            type="button"
-            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 focus:bg-transparent dark:hover:bg-gray-800"
-            data-accordion-target="#accordion-collapse-body-12"
-            aria-expanded="false"
-            aria-controls="accordion-collapse-body-12"
-          >
-            <span class="flex items-center"
-              ><svg
-                class="w-5 h-5 mr-2 shrink-0"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <ContentSlot :use="$slots.title1"
-            /></span>
-            <svg
-              data-accordion-icon
-              class="w-6 h-6 rotate-180 shrink-0"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-        </h2>
-        <div
-          id="accordion-collapse-body-12"
-          class="hidden"
-          aria-labelledby="accordion-collapse-heading-12"
-        >
-          <div
-            class="px-5 pb-2 font-light dark:border-gray-700 dark:bg-gray-900"
-          >
-            <p class="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is an collapse-source library of interactive components
-              built on top of Tailwind CSS including buttons, dropdowns, modals,
-              navbars, and more.
-            </p>
-            <p class="text-gray-500 dark:text-gray-400">
-              Check out this guide to learn how to
-              <a
-                href="/docs/getting-started/introduction/"
-                class="text-blue-600 dark:text-blue-500 hover:underline"
-                >get started</a
-              >
-              and start developing websites even faster with components on top
-              of Tailwind CSS.
-            </p>
-          </div>
-        </div>
-        <h2 id="accordion-collapse-heading-13">
-          <button
-            type="button"
-            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 focus:bg-transparent dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800"
-            data-accordion-target="#accordion-collapse-body-13"
-            aria-expanded="false"
-            aria-controls="accordion-collapse-body-13"
-          >
-            <span class="flex items-center"
-              ><svg
-                class="w-5 h-5 mr-2 shrink-0"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <ContentSlot :use="$slots.title2"
-            /></span>
-            <svg
-              data-accordion-icon
-              class="w-6 h-6 shrink-0"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-        </h2>
-        <div
-          id="accordion-collapse-body-13"
-          class="hidden"
-          aria-labelledby="accordion-collapse-heading-13"
-        >
-          <div class="px-5 pb-2 font-light dark:border-gray-700">
-            <p class="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is first conceptualized and designed using the Figma
-              software so everything you see in the library has a design
-              equivalent in our Figma file.
-            </p>
-            <p class="text-gray-500 dark:text-gray-400">
-              Check out the
-              <a
-                href="https://flowbite.com/figma/"
-                class="text-blue-600 dark:text-blue-500 hover:underline"
-                >Figma design system</a
-              >
-              based on the utility classes from Tailwind CSS and components from
-              Flowbite.
-            </p>
-          </div>
-        </div>
+        <SingleAccordion :num="15" :iconNum="1">
+          <template #title>
+            <ContentSlot :use="$slots.title1" />
+          </template>
+          <template #description1>
+            <ContentSlot :use="$slots.description1" />
+          </template>
+          <template #description2>
+            <ContentSlot :use="$slots.description1_2" />
+          </template>
+        </SingleAccordion>
+        <SingleAccordion :num="13" :iconNum="2">
+          <template #title>
+            <ContentSlot :use="$slots.title2" />
+          </template>
+          <template #description1>
+            <ContentSlot :use="$slots.description2" />
+          </template>
+          <template #description2>
+            <ContentSlot :use="$slots.description2_2" />
+          </template>
+        </SingleAccordion>
 
-        <h2 id="accordion-collapse-heading-14">
-          <button
-            type="button"
-            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 focus:bg-transparent dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800"
-            data-accordion-target="#accordion-collapse-body-14"
-            aria-expanded="false"
-            aria-controls="accordion-collapse-body-14"
-          >
-            <span class="flex items-center"
-              ><svg
-                class="w-5 h-5 mr-2 shrink-0"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <ContentSlot :use="$slots.title3"
-            /></span>
-            <svg
-              data-accordion-icon
-              class="w-6 h-6 shrink-0"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-        </h2>
-        <div
-          id="accordion-collapse-body-14"
-          class="hidden"
-          aria-labelledby="accordion-collapse-heading-14"
-        >
-          <div
-            class="px-5 pb-2 font-light border-gray-200 dark:border-gray-700"
-          >
-            <p class="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is first conceptualized and designed using the Figma
-              software so everything you see in the library has a design
-              equivalent in our Figma file.
-            </p>
-            <p class="text-gray-500 dark:text-gray-400">
-              Check out the
-              <a
-                href="https://flowbite.com/figma/"
-                class="text-blue-600 dark:text-blue-500 hover:underline"
-                >Figma design system</a
-              >
-              based on the utility classes from Tailwind CSS and components from
-              Flowbite.
-            </p>
-          </div>
-        </div>
+        <SingleAccordion :num="14" :iconNum="3">
+          <template #title>
+            <ContentSlot :use="$slots.title3" />
+          </template>
+          <template #description1>
+            <ContentSlot :use="$slots.description3" />
+          </template>
+          <template #description2>
+            <ContentSlot :use="$slots.description3_2" />
+          </template>
+        </SingleAccordion>
 
-        <SingleAccordion :num="15">
+        <SingleAccordion :num="15" :iconNum="4">
           <template #title>
             <ContentSlot :use="$slots.title4" />
           </template>
--- components/content/SingleAccordion.vue
+++ components/content/SingleAccordion.vue
@@ -9,7 +9,7 @@
     >
       <span class="flex items-center">
         <fa-icon
-          class="fa-xl text-[#e74b91] w-5 h-5 mr-2 shrink-0"
+          :class="`fa-xl text-[${iconColor}] w-5 h-5 mr-2 shrink-0`"
           :icon="['fas', `${props.iconNum}`]"
         />
         <slot name="title"></slot>
@@ -48,8 +48,18 @@
 
 <script setup>
 const props = defineProps({
-  num: Number,
-  iconNum: Number,
+  num: {
+    type: Number,
+    required: true,
+  },
+  iconNum: {
+    type: Number,
+    default: 1,
+  },
+  iconColor: {
+    type: String,
+    default: 'black',
+  },
 })
 
 </script>
