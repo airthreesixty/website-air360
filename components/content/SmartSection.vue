3cd271fb4a5f8d1aad9d15f91881a2322690b01d
koki <koki.takahashi@scalefast.com>
2022-12-28 11:54:50 +0900

    replaced the long code to the code with an accodrion component

--- components/content/SmartSection.vue
+++ components/content/SmartSection.vue
@@ -5,262 +5,63 @@
         <h2 class="text-pink-500 text-3xl font-extrabold pb-3">
           <ContentSlot :use="$slots.smart" />
         </h2>
-        <h2 id="accordion-collapse-heading-1">
-          <button
-            type="button"
-            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 focus:bg-transparent dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800"
-            data-accordion-target="#accordion-collapse-body-1"
-            aria-expanded="false"
-            aria-controls="accordion-collapse-body-1"
-          >
-            <span class="flex items-center">
-              <fa-icon
-                class="fa-xl text-[#e74b91] w-5 h-5 mr-2 shrink-0"
-                :icon="['fas', '1']"
-              />
-              <ContentSlot :use="$slots.title1" />
-            </span>
-            <svg
-              data-accordion-icon
-              class="w-6 h-6 rotate-180 shrink-0"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </button>
-        </h2>
-        <div
-          id="accordion-collapse-body-1"
-          class="hidden"
-          aria-labelledby="accordion-collapse-heading-1"
-        >
-          <div
-            class="px-5 pb-2 text-gray-500 font-light dark:border-gray-700 dark:bg-gray-900"
-          >
-            <p class="mb-2 text-gray-500 dark:text-gray-400">
-              <ContentSlot :use="$slots.description1" />
-            </p>
-            <p class="text-gray-500 dark:text-gray-400">
-              <ContentSlot :use="$slots.description1_2" />
-            </p>
-          </div>
-        </div>
-        <h2 id="accordion-collapse-heading-2">
-          <button
-            type="button"
-            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 focus:bg-transparent focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800"
-            data-accordion-target="#accordion-collapse-body-2"
-            aria-expanded="false"
-            aria-controls="accordion-collapse-body-2"
-          >
-            <span class="flex items-center">
-              <svg
-                class="w-5 h-5 mr-2 shrink-0"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <ContentSlot :use="$slots.title2" />
-            </span>
-            <svg
-              data-accordion-icon
-              class="w-6 h-6 shrink-0"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </button>
-        </h2>
-        <div
-          id="accordion-collapse-body-2"
-          class="hidden"
-          aria-labelledby="accordion-collapse-heading-2"
-        >
-          <div class="px-5 pb-2 text-gray-500 font-light dark:border-gray-700">
-            <p class="mb-2 text-gray-500 dark:text-gray-400">
-              <ContentSlot :use="$slots.description2" />
-            </p>
-            <p class="text-gray-500 dark:text-gray-400">
-              <ContentSlot :use="$slots.description2_2" />
-            </p>
-          </div>
-        </div>
+        <SingleAccordion :num="1">
+          <template #title>
+            <ContentSlot :use="$slots.title1" />
+          </template>
+          <template #description1>
+            <ContentSlot :use="$slots.description1" />
+          </template>
+          <template #description2>
+            <ContentSlot :use="$slots.description1_2" />
+          </template>
+        </SingleAccordion>
+        <SingleAccordion :num="2">
+          <template #title>
+            <ContentSlot :use="$slots.title2" />
+          </template>
+          <template #description1>
+            <ContentSlot :use="$slots.description2" />
+          </template>
+          <template #description2>
+            <ContentSlot :use="$slots.description2_2" />
+          </template>
+        </SingleAccordion>
 
-        <h2 id="accordion-collapse-heading-3">
-          <button
-            type="button"
-            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 focus:bg-transparent dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800"
-            data-accordion-target="#accordion-collapse-body-3"
-            aria-expanded="false"
-            aria-controls="accordion-collapse-body-3"
-          >
-            <span class="flex items-center">
-              <svg
-                class="w-5 h-5 mr-2 shrink-0"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <ContentSlot :use="$slots.title3" />
-            </span>
-            <svg
-              data-accordion-icon
-              class="w-6 h-6 shrink-0"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </button>
-        </h2>
-        <div
-          id="accordion-collapse-body-3"
-          class="hidden"
-          aria-labelledby="accordion-collapse-heading-3"
-        >
-          <div class="px-5 pb-2 text-gray-500 font-light dark:border-gray-700">
-            <p class="mb-2 text-gray-500 dark:text-gray-400">
-              <ContentSlot :use="$slots.description3" />
-            </p>
-            <p class="text-gray-500 dark:text-gray-400">
-              <ContentSlot :use="$slots.description3_2" />
-            </p>
-          </div>
-        </div>
+        <SingleAccordion :num="3">
+          <template #title>
+            <ContentSlot :use="$slots.title3" />
+          </template>
+          <template #description1>
+            <ContentSlot :use="$slots.description3" />
+          </template>
+          <template #description2>
+            <ContentSlot :use="$slots.description3_2" />
+          </template>
+        </SingleAccordion>
 
-        <h2 id="accordion-collapse-heading-4">
-          <button
-            type="button"
-            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 focus:bg-transparent dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800"
-            data-accordion-target="#accordion-collapse-body-4"
-            aria-expanded="false"
-            aria-controls="accordion-collapse-body-4"
-          >
-            <span class="flex items-center"
-              ><svg
-                class="w-5 h-5 mr-2 shrink-0"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <ContentSlot :use="$slots.title4"
-            /></span>
-            <svg
-              data-accordion-icon
-              class="w-6 h-6 shrink-0"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-        </h2>
-        <div
-          id="accordion-collapse-body-4"
-          class="hidden"
-          aria-labelledby="accordion-collapse-heading-4"
-        >
-          <div class="px-5 pb-2 text-gray-500 font-light dark:border-gray-700">
-            <p class="mb-2 text-gray-500 dark:text-gray-400">
-              <ContentSlot :use="$slots.description4" />
-            </p>
-            <p class="text-gray-500 dark:text-gray-400">
-              <ContentSlot :use="$slots.description4_2" />
-            </p>
-          </div>
-        </div>
-        <h2 id="accordion-collapse-heading-5">
-          <button
-            type="button"
-            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 focus:bg-transparent dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800"
-            data-accordion-target="#accordion-collapse-body-5"
-            aria-expanded="false"
-            aria-controls="accordion-collapse-body-5"
-          >
-            <span class="flex items-center"
-              ><svg
-                class="w-5 h-5 mr-2 shrink-0"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <ContentSlot :use="$slots.title5"
-            /></span>
-            <svg
-              data-accordion-icon
-              class="w-6 h-6 shrink-0"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-        </h2>
-        <div
-          id="accordion-collapse-body-5"
-          class="hidden"
-          aria-labelledby="accordion-collapse-heading-5"
-        >
-          <div class="px-5 pb-2 text-gray-500 font-light dark:border-gray-700">
-            <p class="mb-2 text-gray-500 dark:text-gray-400">
-              <ContentSlot :use="$slots.description5" />
-            </p>
-            <p class="text-gray-500 dark:text-gray-400">
-              <ContentSlot :use="$slots.description5_2" />
-            </p>
-          </div>
-        </div>
+        <SingleAccordion :num="4">
+          <template #title>
+            <ContentSlot :use="$slots.title4" />
+          </template>
+          <template #description1>
+            <ContentSlot :use="$slots.description4" />
+          </template>
+          <template #description2>
+            <ContentSlot :use="$slots.description4_2" />
+          </template>
+        </SingleAccordion>
+        <SingleAccordion :num="5">
+          <template #title>
+            <ContentSlot :use="$slots.title5" />
+          </template>
+          <template #description1>
+            <ContentSlot :use="$slots.description5" />
+          </template>
+          <template #description2>
+            <ContentSlot :use="$slots.description5_2" />
+          </template>
+        </SingleAccordion>
         <SingleAccordion :num="6">
           <template #title>
             <ContentSlot :use="$slots.title6" />
