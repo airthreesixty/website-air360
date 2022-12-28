556c98bc4ee76ad8f276d0ff140bec1cb556d73b
koki <koki.takahashi@scalefast.com>
2022-12-28 11:32:53 +0900

    Creating an accordion component

---
+++ components/content/SingleAccordion.vue
@@ -0,0 +1,54 @@
+<template>
+  <h2 id="accordion-collapse-heading-1">
+    <button
+      type="button"
+      class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 focus:bg-transparent dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800"
+      data-accordion-target="#accordion-collapse-body-1"
+      aria-expanded="false"
+      aria-controls="accordion-collapse-body-1"
+    >
+      <span class="flex items-center">
+        <fa-icon
+          class="fa-xl text-[#e74b91] w-5 h-5 mr-2 shrink-0"
+          :icon="['fas', `${props.num}`]"
+        />
+        <slot name="title"></slot>
+      </span>
+      <svg
+        data-accordion-icon
+        class="w-6 h-6 rotate-180 shrink-0"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path fill-rule="evenodd"
+          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
+  </h2>
+  <div
+    id="accordion-collapse-body-1"
+    class="hidden"
+    aria-labelledby="accordion-collapse-heading-1"
+  >
+    <div
+      class="px-5 pb-2 text-gray-500 font-light dark:border-gray-700 dark:bg-gray-900"
+    >
+      <p class="mb-2 text-gray-500 dark:text-gray-400">
+        <slot name="description1"></slot>
+      </p>
+      <p class="text-gray-500 dark:text-gray-400">
+        <slot name="description2"></slot>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  num: Number,
+})
+
+</script>
