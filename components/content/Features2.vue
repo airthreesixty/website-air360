1c6eba609c5c8566e554f3bfe1784f626b9fa5bc
koki <koki.takahashi@scalefast.com>
2022-12-29 15:42:54 +0900

    Adjust margin

--- components/content/Features2.vue
+++ components/content/Features2.vue
@@ -4,9 +4,9 @@
       <div class="md:hidden">
         <img src="/boostConversion.png" alt="" loading="lazy">
       </div>
-      <div class="mx-auto max-w-screen-md text-center mb-8 mt-4 flex-1 lg:mb-16">
+      <div class="mx-auto max-w-screen-md text-center mb-8 mt-4 flex-1 md:mb-0">
         <h2 class="mb-4 text-xl tracking-tight font-bold text-black-600 lg:text-2xl xl:text-3xl dark:text-white"><ContentSlot :use="$slots.title" /></h2>
-        <p class="font-light text-gray-500 text-base lg:text-lg xl:text-xl dark:text-gray-400"><ContentSlot :use="$slots.description" /></p>
+        <p class="font-light text-gray-500 text-base text-left lg:text-lg xl:text-xl dark:text-gray-400"><ContentSlot :use="$slots.description" /></p>
       </div>
       <div class="flex-1 hidden md:block">
         <img src="/boostConversion.png" alt="" loading="lazy">
