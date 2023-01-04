834228eaae7fce4e0c744c071f94013a8c286e6d
koki <koki.takahashi@scalefast.com>
2023-01-04 11:20:15 +0900

    Added the grayish background

--- components/content/Description2.vue
+++ components/content/Description2.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bg-white dark:bg-gray-900">
+  <section class="bg-gray-50 dark:bg-gray-900">
     <div
       class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12"
     >
