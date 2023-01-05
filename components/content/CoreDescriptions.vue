e2911b32a0667a846a03d599eec11792f61af31c
koki <koki.takahashi@scalefast.com>
2023-01-05 11:57:09 +0900

    Created description component and set prop for bgcolor

---
+++ components/content/CoreDescriptions.vue
@@ -0,0 +1,26 @@
+<template>
+  <SingleDescription>
+    <template #title>
+      <ContentSlot :use="$slots.title1" />
+    </template>
+    <template #description>
+      <ContentSlot :use="$slots.description1" />
+    </template>
+  </SingleDescription>
+  <SingleDescription bg-color="gray-50">
+    <template #title>
+      <ContentSlot :use="$slots.title2" />
+    </template>
+    <template #description>
+      <ContentSlot :use="$slots.description2" />
+    </template>
+  </SingleDescription>
+  <SingleDescription>
+    <template #title>
+      <ContentSlot :use="$slots.title3" />
+    </template>
+    <template #description>
+      <ContentSlot :use="$slots.description3" />
+    </template>
+  </SingleDescription>
+</template>
