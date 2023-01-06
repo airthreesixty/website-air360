96a2f0328ddc233230b0467f986c1c4f46e43fbc
koki <koki.takahashi@scalefast.com>
2023-01-06 17:44:38 +0900

    Create whyair360 component

---
+++ components/content/WhyAir360Description.vue
@@ -0,0 +1,113 @@
+<template>
+  <section :class="`bg-${props.bgColor} dark:bg-gray-900`">
+    <div
+      class="grid max-w-screen-[1400px] px-4 py-8 mx-auto md:gap-8 xl:gap-0 md:py-16 md:grid-cols-12"
+    >
+      <div :class="`md:${props.imageStatus} md:mt-0 md:col-span-5 md:flex`">
+        <img
+          :src="imagePath"
+          alt="mockup"
+        >
+      </div>
+      <div class="mr-auto place-self-center md:col-span-7">
+        <p class="text-primary-600 font-bold text-lg my-2">
+          <slot name="subtext" />
+        </p>
+        <h1
+          class="mb-4 text-xl tracking-tight font-semibold text-black-600 text-left md:text-2xl lg:text-3xl dark:text-white"
+        >
+          <slot name="title" />
+        </h1>
+        <div class="flex">
+          <div
+            class="text-gray-700"
+          >
+            <fa-icon class="fa-xl" :icon="['fab', 'twitter']" />
+            <span class="sr-only">Twitter page</span>
+          </div>
+          <p
+            class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-base lg:text-xl dark:text-gray-400"
+          >
+            <slot name="description1" />
+          </p>
+        </div>
+        <div class="flex">
+          <div
+            class="text-gray-700"
+          >
+            <fa-icon class="fa-xl" :icon="['fab', 'twitter']" />
+            <span class="sr-only">Twitter page</span>
+          </div>
+          <p
+            class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-base lg:text-xl dark:text-gray-400"
+          >
+            <slot name="description2" />
+          </p>
+        </div>
+        <div class="flex">
+          <div
+            class="text-gray-700"
+          >
+            <fa-icon class="fa-xl" :icon="['fab', 'twitter']" />
+            <span class="sr-only">Twitter page</span>
+          </div>
+          <p
+            class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-base lg:text-xl dark:text-gray-400"
+          >
+            <slot name="description3" />
+          </p>
+        </div>
+        <a
+          href="#"
+          class="items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-400 transition ease-in-out duration-300 hover:bg-blue-500 dark:focus:ring-primary-900"
+          :class="buttonStatus"
+        >
+          {{ $t("more") }}
+          <svg
+            class="w-5 h-5 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
+      </div>
+      <div :class="`hidden md:${props.imageStatus2} md:mt-0 md:col-span-5 md:flex`">
+        <img
+          :src="imagePath"
+          alt="mockup"
+        >
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({
+  bgColor: {
+    type: String,
+    default: 'white',
+  },
+  imagePath: {
+    type: String,
+    required: true,
+  },
+  imageStatus: {
+    type: String,
+    default: 'hidden',
+  },
+  imageStatus2: {
+    type: String,
+    default: 'block',
+  },
+  buttonStatus: {
+    type: String,
+    default: 'inline-flex',
+  },
+})
+</script>
--- content/ja/why-air360.md
+++ content/ja/why-air360.md
@@ -39,6 +39,19 @@ great solution
 ピクセルをHTMLに埋め込むだけなので、すぐ分析を始められてストレスフリー。更には過去のデータも遡れ、データの取り逃がしがありません。
 ::
 
+::why-air360-description{imagePath="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" imageStatus="block" imageStatus2="hidden" buttonStatus="hidden"}
+#title
+ユーザーの動きを全てトラッキング
+#subtext
+great solution
+#description1
+ユーザーの離脱点が分かる
+#description2
+ユーザーの離脱点が分かる
+#description3
+ユーザーの離脱点が分かる
+::
+
 <!-- ::company-logos
 #title
 愛される E コマースブランドが Air360 を採用しています
