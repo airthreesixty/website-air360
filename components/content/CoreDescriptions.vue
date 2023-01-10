d0784d02db163fc24ce5536203dc6aa9f567f3ff
koki <koki.takahashi@scalefast.com>
2023-01-10 14:02:44 +0900

    Replaced picutures

--- components/content/CoreDescriptions.vue
+++ components/content/CoreDescriptions.vue
@@ -1,5 +1,5 @@
 <template>
-  <SingleDescription image-path="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png">
+  <SingleDescription image-path="http://air360-website.s3-website-eu-west-1.amazonaws.com/assets/images/replays-2-764x478.jpeg">
     <template #title>
       <ContentSlot :use="$slots.title1" />
     </template>
@@ -7,7 +7,7 @@
       <ContentSlot :use="$slots.description1" />
     </template>
   </SingleDescription>
-  <SingleDescription bg-color="gray-50" image-status="block" image-status2="hidden" image-path="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png">
+  <SingleDescription bg-color="gray-50" image-status="block" image-status2="hidden" image-path="http://air360-website.s3-website-eu-west-1.amazonaws.com/assets/images/event-stats-1212x758.jpg">
     <template #title>
       <ContentSlot :use="$slots.title2" />
     </template>
@@ -15,7 +15,7 @@
       <ContentSlot :use="$slots.description2" />
     </template>
   </SingleDescription>
-  <SingleDescription image-path="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png">
+  <SingleDescription image-path="/dataIntegration.png">
     <template #title>
       <ContentSlot :use="$slots.title3" />
     </template>
