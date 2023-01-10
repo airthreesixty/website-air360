e1b72c263cd32e9a3dfd5403cf60539cdea70e0a
koki <koki.takahashi@scalefast.com>
2023-01-10 20:25:48 +0900

    Centered

--- components/content/WhyPeopleChooseAir360.vue
+++ components/content/WhyPeopleChooseAir360.vue
@@ -7,14 +7,14 @@
         Each season, we collaborate with world-class designers to create a collection inspired by the natural world.
       </p> -->
       <div class="mt-10 space-y-12 md:grid md:grid-cols-3 md:space-y-0">
-        <a v-for="category in categories" :key="category.name" :href="category.href" class="group block">
+        <div v-for="category in categories" :key="category.name" class="group block">
           <div aria-hidden="true" class="aspect-w-3 aspect-h-2 overflow-hidden lg:aspect-w-5 lg:aspect-h-6">
             <img :src="category.imageSrc" :alt="category.imageAlt" class="h-[250px] w-full object-cover object-center">
           </div>
           <div class="px-1 md:px-2">
             <h3 class="mt-4 text-xl font-semibold text-black-600 lg:text-2xl">{{ category.name }}</h3>
             <p class="mt-2 text-lg text-gray-500">{{ category.description }}</p>
-            <div class="flex text-primary-600 mt-3 ">
+            <a href="#" class="flex text-primary-600 mt-3 items-center">
               <p>{{ $t("more") }}
               </p>
               <svg
@@ -29,9 +29,9 @@
                   clip-rule="evenodd"
                 />
               </svg>
-            </div>
+            </a>
           </div>
-        </a>
+        </div>
       </div>
     </div>
   </div>
@@ -41,21 +41,18 @@
 const categories = [
   {
     name: 'スマート',
-    href: '#',
     imageSrc: '/smartDescription.png',
     imageAlt: 'Brown leather key ring with brass metal loops and rivets on wood table.',
     description: '視覚的な情報や事前通知を駆使し、あなたの意思決定をアシスタントします。',
   },
   {
     name: '直感的',
-    href: '#',
     imageSrc: '/intuitiveDescription.png',
     imageAlt: 'Natural leather mouse pad on white desk next to porcelain mug and keyboard.',
     description: '優れた操作性を備え持ち、コードレス&タグレスですぐに分析を始められます。',
   },
   {
     name: '連携性',
-    href: '#',
     imageSrc: '/dataIntegration.png',
     imageAlt: 'Person placing task list card into walnut card holder next to felt carrying case on leather desk pad.',
     description: '豊富な種類の外部ツールとの連携が可能なオープンエコシステムなアプローチを採用しています。',
