9a5a4b0c49fb27db23ffd8293765becc7d14a052
koki <koki.takahashi@scalefast.com>
2023-01-10 16:48:18 +0900

    Fix indentation

--- components/content/CtaNew.vue
+++ components/content/CtaNew.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="bg-white">
-      <div class="max-w-screen-[1400px] mx-auto py-16 px-6 lg:px-8">
-        <div class="overflow-hidden rounded-lg bg-primary-600 shadow-xl lg:grid lg:grid-cols-2 lg:gap-4">
-          <div class="px-6 pt-10 pb-12 sm:px-16 sm:pt-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
-            <div class="lg:self-center">
-              <h2 class="text-2xl font-bold tracking-tight text-white md:text-3xl lg:text-4xl">
-                <span class="block">今すぐ始めましょう</span>
-              </h2>
+  <div class="bg-white">
+    <div class="max-w-screen-[1400px] mx-auto py-16 px-6 lg:px-8">
+      <div class="overflow-hidden rounded-lg bg-primary-600 shadow-xl lg:grid lg:grid-cols-2 lg:gap-4">
+        <div class="px-6 pt-10 pb-12 sm:px-16 sm:pt-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
+          <div class="lg:self-center">
+            <h2 class="text-2xl font-bold tracking-tight text-white md:text-3xl lg:text-4xl">
+              <span class="block">今すぐ始めましょう</span>
+            </h2>
             <p class="mt-4 text-md leading-6 text-primary-100 lg:text-xl">
               ユーザー体験を改善し、コンバージョンを上げてみませんか？Air360なら一行のコードを追加するだけで分析を始められ、素早くインサイトを得られます。
             </p>
-              <a href="#" class="mt-8 inline-flex items-center rounded-lg border border-transparent bg-white px-6 py-3 text-base font-medium text-primary-600 shadow hover:bg-primary-100">Air360を試してみる</a>
-            </div>
+            <a href="#" class="mt-8 inline-flex items-center rounded-lg border border-transparent bg-white px-6 py-3 text-base font-medium text-primary-600 shadow hover:bg-primary-100">Air360を試してみる</a>
           </div>
-          <div class="aspect-w-5 aspect-h-3 -mt-6 md:aspect-w-2 md:aspect-h-1">
+        </div>
+        <div class="aspect-w-5 aspect-h-3 -mt-6 md:aspect-w-2 md:aspect-h-1">
           <img class="translate-x-6 translate-y-6 transform rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20" src="https://tailwindui.com/img/component-images/full-width-with-sidebar.jpg" alt="App screenshot" >
-          </div>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
