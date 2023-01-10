d4c8bc204de9cc5368144d5742958dce9df7ec1d
koki <koki.takahashi@scalefast.com>
2023-01-10 14:12:03 +0900

    Imported the image and use them

--- components/content/CoreDescriptions.vue
+++ components/content/CoreDescriptions.vue
@@ -1,5 +1,5 @@
 <template>
-  <SingleDescription image-path="http://air360-website.s3-website-eu-west-1.amazonaws.com/assets/images/replays-2-764x478.jpeg">
+  <SingleDescription image-path="/smartDescription.png">
     <template #title>
       <ContentSlot :use="$slots.title1" />
     </template>
@@ -7,7 +7,7 @@
       <ContentSlot :use="$slots.description1" />
     </template>
   </SingleDescription>
-  <SingleDescription bg-color="gray-50" image-status="block" image-status2="hidden" image-path="http://air360-website.s3-website-eu-west-1.amazonaws.com/assets/images/event-stats-1212x758.jpg">
+  <SingleDescription bg-color="gray-50" image-status="block" image-status2="hidden" image-path="/intuitiveDescription.png">
     <template #title>
       <ContentSlot :use="$slots.title2" />
     </template>
