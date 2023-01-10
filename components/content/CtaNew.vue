03aa9bd98571b3a630b0a0961eaffed97a61f379
koki <koki.takahashi@scalefast.com>
2023-01-10 16:24:36 +0900

    Ajust font size and max-witdh

--- components/content/CtaNew.vue
+++ components/content/CtaNew.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="bg-white">
-      <div class="mx-auto max-w-7xl py-16 px-6 lg:px-8">
+      <div class="max-w-screen-[1400px] mx-auto py-16 px-6 lg:px-8">
         <div class="overflow-hidden rounded-lg bg-primary-600 shadow-xl lg:grid lg:grid-cols-2 lg:gap-4">
           <div class="px-6 pt-10 pb-12 sm:px-16 sm:pt-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
             <div class="lg:self-center">
-              <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              <h2 class="text-2xl font-bold tracking-tight text-white md:text-3xl lg:text-4xl">
                 <span class="block">今すぐ始めましょう</span>
               </h2>
-            <p class="mt-4 text-lg leading-6 text-primary-100">
+            <p class="mt-4 text-md leading-6 text-primary-100 lg:text-xl">
               ユーザー体験を改善し、コンバージョンを上げてみませんか？Air360なら一行のコードを追加するだけで分析を始められ、素早くインサイトを得られます。
             </p>
               <a href="#" class="mt-8 inline-flex items-center rounded-lg border border-transparent bg-white px-6 py-3 text-base font-medium text-primary-600 shadow hover:bg-primary-100">Air360を試してみる</a>
