f755cc8897b9ca8569858ddc88aa6228d41a1d36
koki <koki.takahashi@scalefast.com>
2023-01-11 22:04:35 +0900

    Ajust title size

--- components/content/WhyAir360.vue
+++ components/content/WhyAir360.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="my-8 py-3 text-black-600 text-3xl text-center font-bold tracking-tight leading-none  md:text-4xl md:mt-15 lg:text-5xl lg:py-4 dark:text-white">
+  <h1 class="my-8 py-3 text-black-600 text-2xl text-center font-bold md:text-3xl md:mt-15 lg:text-4xl lg:py-4 dark:text-white">
     Why Air360?
   </h1>
 </template>
