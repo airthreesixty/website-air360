82513559ea3c5c5a34e11db20457b8f8738bbe61
koki <koki.takahashi@scalefast.com>
2023-01-11 13:31:18 +0900

    Create what is air360 component

---
+++ components/content/WhatIsAir360.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="relative overflow-hidden bg-gray-50 pt-24 sm:pt-32 lg:pt-40">
+    <div class="mx-auto max-w-md px-6 text-center sm:max-w-3xl lg:max-w-7xl lg:px-8">
+      <div>
+        <h2 class="text-lg font-semibold text-indigo-600">Serverless</h2>
+        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">No server? No problem.</p>
+        <p class="mx-auto mt-5 max-w-prose text-xl text-gray-600">Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.</p>
+      </div>
+      <div class="mt-12 -mb-10 sm:-mb-24 lg:-mb-80">
+        <img class="rounded-lg shadow-xl ring-1 ring-black ring-opacity-5" src="https://tailwindui.com/img/component-images/top-nav-with-multi-column-layout-screenshot.jpg" alt="" >
+      </div>
+    </div>
+  </div>
+</template>
