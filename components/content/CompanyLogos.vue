3d8a768f5e77e3104158264afd79ee365f3b66f9
koki <koki.takahashi@scalefast.com>
2023-01-11 11:00:43 +0900

    Adjust gap and padding

--- components/content/CompanyLogos.vue
+++ components/content/CompanyLogos.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="bg-white dark:bg-gray-900">
-    <div class="pb-8 lg:pb-20 mx-auto max-w-screen-[1400px] px-4">
+    <div class="pb-16 lg:pb-20 mx-auto max-w-screen-[1400px] px-4">
       <h2
         class="mb-10 text-black-600 lg:mb-16 lg:text-2xl text-xl font-bold tracking-tight leading-tight text-center dark:text-white md:text-2xl"
       >
         <ContentSlot :use="$slots.title" />
       </h2>
       <div
-        class="grid grid-cols-2 gap-10 text-gray-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400"
+        class="grid grid-cols-2 gap-12 text-gray-500 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400"
       >
         <div class="flex justify-center items-center">
           <img
