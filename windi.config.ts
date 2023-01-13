8eab0bb5e39999ec18e72f0c9070bcbff87ce649
koki <koki.takahashi@scalefast.com>
2023-01-13 15:32:02 +0900

    organize code

--- windi.config.ts
+++ windi.config.ts
@@ -94,20 +94,6 @@ export default defineConfig({
           600: '#232e4a',
         },
       },
-      keyframes: {
-        slide1: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-        slide2: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-200%)' },
-        },
-      },
-      animation: {
-        slideshow1: 'slide1 60s -30s linear infinite',
-        slideshow2: 'slide2 60s linear infinite',
-      },
     },
   },
 })
