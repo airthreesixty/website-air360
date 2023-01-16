ca1d658fa83a0177b27a815616f57e7b61ab63d4
koki <koki.takahashi@scalefast.com>
2023-01-16 10:36:29 +0900

    Change div to section

--- components/content/CtaNew.vue
+++ components/content/CtaNew.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white">
+  <section class="bg-white">
     <div class="max-w-screen-[1400px] mx-auto pb-16 pt-10 px-4 lg:px-8">
       <div class="overflow-hidden rounded-lg bg-primary-600 shadow-xl lg:grid lg:grid-cols-2 lg:gap-4">
         <div class="px-6 pt-10 pb-12 md:px-16 md:pt-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
@@ -18,5 +18,5 @@
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
