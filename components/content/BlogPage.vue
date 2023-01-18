5233859354ca3a26f29a247739847503eb8080ad
koki <koki.takahashi@scalefast.com>
2023-01-18 16:59:30 +0900

    Adjust font size and color

--- components/content/BlogPage.vue
+++ components/content/BlogPage.vue
@@ -3,10 +3,13 @@
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
         <h2
-          class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
+          class="mb-4 text-3xl tracking-tight font-extrabold text-black-600 lg:text-4xl dark:text-white"
         >
           Air360 ブログ
         </h2>
+        <h3 class="text-xl text-black-600 md:text-2xl">
+          Hello
+        </h3>
         <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
           Inspiration for conversion heroes
         </p>
