74d9085bdb7ef0c8f5209428ba59a6feb0eb318f
koki <koki.takahashi@scalefast.com>
2023-01-18 11:27:23 +0900

    Replace the share icons and be responsive for mobile

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -8,14 +8,75 @@
             <article
               class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert"
             >
-              <nuxt-link
-                v-for="(tag, index) in data.tags"
-                :key="index"
-                class="bg-blue-100 text-blue-400 text-xs font-semibold px-2.5 py-0.5 mr-2 rounded dark:bg-purple-200 dark:text-purple-900"
-                :to="$localePath(`/blog/tag/${tag}`)"
-              >
-                {{ $t(`tag.${tag}`) }}
-              </nuxt-link>
+              <div class="flex items-center justify-between">
+                <nuxt-link
+                  v-for="(tag, index) in data.tags"
+                  :key="index"
+                  class="bg-blue-100 text-blue-400 text-xs font-semibold px-2.5 py-0.5 mr-2 rounded dark:bg-purple-200 dark:text-purple-900"
+                  :to="$localePath(`/blog/tag/${tag}`)"
+                >
+                  {{ $t(`tag.${tag}`) }}
+                </nuxt-link>
+                <aside aria-label="Share social media">
+                  <div class="not-format flex items-center">
+                    <p class="text-primary-600">
+                      Share:
+                    </p>
+                    <button data-tooltip-target="tooltip-linkedin" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
+                      <a
+                        href="#"
+                        class="text-primary-600 transition ease-in-out duration-300 hover:text-primary-700 dark:hover:text-white"
+                      >
+                        <fa-icon class="fa-xl" :icon="['fab', 'linkedin']" />
+                        <span class="sr-only">LinkedIn page</span>
+                      </a>
+                    </button>
+                    <div id="tooltip-linkedin" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+                      Share on Linkedin
+                      <div class="tooltip-arrow" data-popper-arrow />
+                    </div>
+                    <button data-tooltip-target="tooltip-twitter" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
+                      <a
+                        href="#"
+                        class="text-primary-600 transition ease-in-out duration-300 hover:text-primary-700 dark:hover:text-white"
+                      >
+                        <fa-icon class="fa-xl" :icon="['fab', 'twitter']" />
+                        <span class="sr-only">LinkedIn page</span>
+                      </a>
+                    </button>
+                    <div id="tooltip-twitter" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+                      Share on Twitter
+                      <div class="tooltip-arrow" data-popper-arrow />
+                    </div>
+                    <button data-tooltip-target="tooltip-facebook" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
+                      <a
+                        href="#"
+                        class="text-primary-600 transition ease-in-out duration-300 hover:text-primary-700 dark:hover:text-white"
+                      >
+                        <fa-icon class="fa-xl" :icon="['fab', 'facebook']" />
+                        <span class="sr-only">Facebook page</span>
+                      </a>
+                    </button>
+                    <div id="tooltip-facebook" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+                      Share on Facebook
+                      <div class="tooltip-arrow" data-popper-arrow />
+                    </div>
+                    <button data-tooltip-target="tooltip-link" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
+                      <a
+                        href="#"
+                        class="text-primary-600 transition ease-in-out duration-300 hover:text-primary-700 dark:hover:text-white"
+                      >
+                        <fa-icon class="fa-xl" :icon="['fas', 'link']" />
+                        <span class="sr-only">Link page</span>
+                      </a>
+                    </button>
+                    <div id="tooltip-link" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+                      Share link
+                      <div class="tooltip-arrow" data-popper-arrow />
+                    </div>
+                  </div>
+                </aside>
+              </div>
               <header class="my-4 lg:mb-6 not-format">
                 <div class="flex items-center justify-between mb-6 not-italic">
                   <div
@@ -43,65 +104,6 @@
                       </p>
                     </div>
                   </div>
-                  <aside aria-label="Share social media">
-                    <div class="not-format flex items-center">
-                      <p class="text-primary-600">
-                        Share:
-                      </p>
-                      <button data-tooltip-target="tooltip-linkedin" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
-                        <a
-                          href="#"
-                          class="text-primary-600 transition ease-in-out duration-300 hover:text-primary-700 dark:hover:text-white"
-                        >
-                          <fa-icon class="fa-xl" :icon="['fab', 'linkedin']" />
-                          <span class="sr-only">LinkedIn page</span>
-                        </a>
-                      </button>
-                      <div id="tooltip-linkedin" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
-                        Share on Linkedin
-                        <div class="tooltip-arrow" data-popper-arrow />
-                      </div>
-                      <button data-tooltip-target="tooltip-twitter" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
-                        <a
-                          href="#"
-                          class="text-primary-600 transition ease-in-out duration-300 hover:text-primary-700 dark:hover:text-white"
-                        >
-                          <fa-icon class="fa-xl" :icon="['fab', 'twitter']" />
-                          <span class="sr-only">LinkedIn page</span>
-                        </a>
-                      </button>
-                      <div id="tooltip-twitter" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
-                        Share on Twitter
-                        <div class="tooltip-arrow" data-popper-arrow />
-                      </div>
-                      <button data-tooltip-target="tooltip-facebook" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
-                        <a
-                          href="#"
-                          class="text-primary-600 transition ease-in-out duration-300 hover:text-primary-700 dark:hover:text-white"
-                        >
-                          <fa-icon class="fa-xl" :icon="['fab', 'facebook']" />
-                          <span class="sr-only">Facebook page</span>
-                        </a>
-                      </button>
-                      <div id="tooltip-facebook" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
-                        Share on Facebook
-                        <div class="tooltip-arrow" data-popper-arrow />
-                      </div>
-                      <button data-tooltip-target="tooltip-link" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
-                        <a
-                          href="#"
-                          class="text-primary-600 transition ease-in-out duration-300 hover:text-primary-700 dark:hover:text-white"
-                        >
-                          <fa-icon class="fa-xl" :icon="['fas', 'link']" />
-                          <span class="sr-only">Link page</span>
-                        </a>
-                      </button>
-                      <div id="tooltip-link" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
-                        Share link
-                        <div class="tooltip-arrow" data-popper-arrow />
-                      </div>
-                    </div>
-                  </aside>
                 </div>
                 <h1
                   class="mb-4 text-3xl font-semibold text-black-600 lg:mb-6 lg:text-4xl dark:text-white"
