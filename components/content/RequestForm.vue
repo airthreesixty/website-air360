40abdd517871bdaaab1a7e1d8b2b7ebf9bdba4ff
koki <koki.takahashi@scalefast.com>
2023-01-20 18:00:08 +0900

    Change the input type of email to text to remove the bubble

--- components/content/RequestForm.vue
+++ components/content/RequestForm.vue
@@ -152,10 +152,11 @@
               <input
                 id="email"
                 v-model="formData.email"
-                type="email"
+                type="text"
                 name="email"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg transition ease-in-out duration-300 focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 autocomplete="email"
+                novalidate
               >
             </div>
             <span v-if="v$.email.$error" class="error-alert">
