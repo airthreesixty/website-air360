bc7938fa49c55f6ddadaea67a766e4e701a7d928
koki <koki.takahashi@scalefast.com>
2023-01-20 17:18:05 +0900

    Translate validation messages

--- components/content/RequestForm.vue
+++ components/content/RequestForm.vue
@@ -235,8 +235,7 @@
 
 <script setup lang="ts">
 import { useVuelidate } from '@vuelidate/core'
-import { required, email, minLength, maxLength, numeric } from '@vuelidate/validators'
-import { createI18n } from 'vue-i18n'
+import { required, email, numeric } from '~/utils/i18n-validators'
 
 const formData = reactive({
   name: '',
@@ -250,11 +249,9 @@ const rules = computed(() => {
     name: { required },
     email: { required, email },
     jobTitle: { required },
-    phoneNumber: { required, minLength: minLength(9), maxLength: maxLength(14), numeric: numeric },
+    phoneNumber: { required, numeric },
   }
 })
-
-// const
 
 const v$ = useVuelidate(rules, formData)
 
---
+++ utils/i18n-validators.js
@@ -0,0 +1,26 @@
+import * as validators from '@vuelidate/validators'
+import { createI18n } from 'vue-i18n'
+
+const messages = {
+  ja: {
+    validations: {
+      required: 'この項目は必須です。',
+      email: '正しいメールアドレスの形式で入力してください。',
+      numeric: '数字で入力してください。',
+    },
+  },
+}
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'ja',
+  messages,
+})
+
+const { createI18nMessage } = validators
+
+const withI18nMessage = createI18nMessage({ t: i18n.global.t.bind(i18n) })
+
+export const required = withI18nMessage(validators.required)
+export const email = withI18nMessage(validators.email)
+export const numeric = withI18nMessage(validators.numeric)
