ec73c0371a692275877539cdd82567b8ad6f2bd4
koki <koki.takahashi@scalefast.com>
2023-01-24 14:57:18 +0900

    Adjustment

--- components/content/ESW.vue
+++ components/content/ESW.vue
@@ -1,47 +1,57 @@
 <!-- This example requires Tailwind CSS v3.0+ -->
 <template>
-  <div class="relative bg-white">
-    <div
-      class="relative h-80 overflow-hidden bg-indigo-600 md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2"
-    >
-      <img
-        class="h-full w-full object-cover"
-        src="https://esw.com/wp-content/uploads/2022/12/dtc_a.jpg"
-        alt=""
-      >
-    </div>
-    <div class="relative mx-auto max-w-7xl py-24 sm:py-32 lg:py-40 lg:px-8">
+  <section>
+    <div class="relative max-w-screen-[1400px] mx-auto bg-white">
       <div
-        class="pr-6 pl-6 md:ml-auto md:w-2/3 md:pl-16 lg:w-1/2 lg:pr-0 xl:pl-32"
+        class="h-80 overflow-hidden md:absolute md:left-0 md:h-full md:w-1/2"
       >
-        <!-- <h2 class="text-base font-semibold leading-7 text-indigo-400">
-          Award winning support
-        </h2> -->
-        <div class="grid grid-cols-12 gird-rows-3">
-          <p class="col-start-1 col-end-7 mt-2 text-4xl font-bold tracking-tight text-black-600">
-            私たちは
-          </p>
-          <div class="col-start-4 col-end-9 row-start-2 md:col-start-5 md:col-end-10 lg:col-start-4">
-            <img
-              src="https://esw.com/wp-content/themes/esw_com/img/logo_esw_a.svg"
-              alt="esw Logo"
-              class="w-40 py-8"
+        <img
+          class="h-full w-full object-cover"
+          src="https://esw.com/wp-content/uploads/2022/12/dtc_a.jpg"
+          alt=""
+        >
+      </div>
+      <div class="relative py-24 md:py-32 lg:py-40 lg:px-8">
+        <div class="pr-6 pl-6 md:ml-auto md:pl-16 md:w-1/2 lg:pr-0 xl:pl-32">
+          <!-- <h2 class="text-base font-semibold leading-7 text-indigo-400">
+              Award winning support
+            </h2> -->
+          <div class="grid grid-cols-12 gird-rows-3">
+            <p
+              class="col-start-1 col-end-7 mt-2 text-4xl font-bold tracking-tight text-black-600"
             >
+              私たちは
+            </p>
+            <div
+              class="col-start-4 col-end-9 row-start-2 md:col-start-4 md:col-end-9 lg:col-start-4"
+            >
+              <img
+                src="https://esw.com/wp-content/themes/esw_com/img/logo_esw_a.svg"
+                alt="esw Logo"
+                class="w-40 py-8"
+              >
+            </div>
+            <p
+              class="col-start-6 col-end-13 md:col-start-5 lg:col-start-7 lg:col-end-14 row-start-3 mt-2 text-4xl font-bold tracking-tight text-black-600"
+            >
+              の一員です
+            </p>
           </div>
-          <p class="col-start-6 col-end-13 md:col-start-8 lg:col-start-7 lg:col-end-14 row-start-3 mt-2 text-4xl font-bold tracking-tight text-black-600">
-            の一員です
-          </p>
-        </div>
-        <!-- <p class="mt-6 text-base leading-7 text-gray-300">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas
-          tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim
-          et fermentum, augue. Aliquet amet volutpat quisque ut interdum
-          tincidunt duis.
-        </p> -->
-        <div class="mt-8">
-          <a href="https://esw.com/" target="_blank" class="text-white bg-primary-600 text-base font-bold mt-5 inline-block transition ease-in-out duration-300 hover:bg-primary-700 font-medium rounded-lg text-sm px-6 py-3 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">ESWのホームページ</a>
+          <!-- <p class="mt-6 text-base leading-7 text-gray-300">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas
+              tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim
+              et fermentum, augue. Aliquet amet volutpat quisque ut interdum
+              tincidunt duis.
+            </p> -->
+          <div class="mt-8">
+            <a
+              href="https://esw.com/"
+              target="_blank"
+              class="text-white bg-primary-600 text-base font-bold mt-5 inline-block transition ease-in-out duration-300 hover:bg-primary-700 font-medium rounded-lg text-sm px-6 py-3 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >ESWのホームページ</a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
