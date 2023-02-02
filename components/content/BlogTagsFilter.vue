e2094c197f84ff702813800d72091c35013c3a95
koki <koki.takahashi@scalefast.com>
2023-02-02 17:48:32 +0900

    Add the active color

--- components/content/BlogTagsFilter.vue
+++ components/content/BlogTagsFilter.vue
@@ -3,7 +3,7 @@
     v-for="tag in tagList"
     :key="tag"
     :to="$localePath(`/blog/tag/${tag}`)"
-    class="py-3 px-5 mt-5 inline-block text-medium lg:text-lg bg-blue-100 text-blue-400 font-semibold rounded mx-2"
+    class="tag__link"
   >
     {{ $t(`tag.${tag}`) }}
   </nuxt-link>
@@ -13,3 +13,13 @@
 const { $localePath } = useNuxtApp()
 const tagList = ['all', 'marketing', 'ux', 'ecommerce', 'other']
 </script>
+
+<style lang="postcss">
+.tag__link {
+  @apply py-3 px-5 mt-5 inline-block text-base lg:text-lg bg-blue-100 text-blue-400 font-semibold rounded mx-2;
+}
+
+.router-link-exact-active {
+  @apply text-white bg-blue-400;
+}
+</style>
