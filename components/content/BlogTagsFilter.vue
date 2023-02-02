d03d8d418a8531200d9b0bc6e9aa79dbcee066d1
koki <koki.takahashi@scalefast.com>
2023-02-02 17:37:56 +0900

    Clean code

--- components/content/BlogTagsFilter.vue
+++ components/content/BlogTagsFilter.vue
@@ -1,7 +1,4 @@
 <template>
-  <!-- <nuxt-link :to="$localePath(`/blog/tag/all`)" class="py-3 px-5 mt-5 inline-block text-medium lg:text-lg bg-blue-100 text-blue-400 font-semibold rounded mx-2">
-    {{ $t('tag.all') }}
-  </nuxt-link> -->
   <nuxt-link
     v-for="tag in tagList"
     :key="tag"
