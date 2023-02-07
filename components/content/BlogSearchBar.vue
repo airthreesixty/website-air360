b97f20bdcf219292126b5801fab2e5725143daf2
koki <koki.takahashi@scalefast.com>
2023-02-07 17:21:53 +0900

    remove the placeholder

--- components/content/BlogSearchBar.vue
+++ components/content/BlogSearchBar.vue
@@ -18,14 +18,13 @@
         v-model="filterText"
         type="search"
         class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:border-primary-600 focus:ring-primary-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Search Mockups, Logos..."
       >
       <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-primary-600 hover:bg-primary-700 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Search
       </button>
     </div>
   </form>
-  <!-- <div>{{ result }}</div> -->
+  <div>{{ result }}</div>
 </template>
 
 <script lang="ts" setup>
