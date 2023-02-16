cd341a70c2d56b48f080ffe2f02c183195622b9f
koki <koki.takahashi@scalefast.com>
2023-02-16 14:39:06 +0900

    Add a catch phrase to the title

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
 import { useSeoMeta } from '@unhead/vue'
 
 useHead({
-  title: 'Air360',
+  title: 'Air360 - eコマースに最適なUX分析ツール',
   titleTemplate: '',
   meta: [
     { hid: 'description', name: 'description', content: 'Air360はウェブサイト上の顧客体験を分析するツールです。 データによって隠れているユーザーの行動を明らかにし、それに基づいてコンバージョン率を改善します。' },
