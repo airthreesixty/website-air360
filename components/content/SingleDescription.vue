fb8c9068ac9353c7f2f0948c179c90e69054a2b0
koki <koki.takahashi@scalefast.com>
2023-03-10 16:07:36 +0900

    Adjust the image

--- components/content/SingleDescription.vue
+++ components/content/SingleDescription.vue
@@ -4,7 +4,7 @@
       class="grid max-w-screen-[1400px] px-4 py-10 mx-auto md:gap-8 md:py-16 md:grid-cols-12 lg:py-18 lg:px-8"
     >
       <div :class="`flex justify-center md:${props.imageStatus} md:mt-0 md:col-span-7 md:flex md:mx-auto lg:w-10/12`">
-        <nuxt-img format="webp" fit="cover" :src="props.imagePath" :alt="props.alt" class="rounded-lg shadow-lg my-auto h-46 w-80 lg:h-69 lg:w-120 xl:h-76 xl:w-132" />
+        <nuxt-img format="webp" :src="props.imagePath" :alt="props.alt" class="rounded-lg shadow-lg my-auto h-46 w-80 object-cover lg:h-69 lg:w-120 xl:h-76 xl:w-132" />
       </div>
       <div class="md:col-span-5">
         <p class="font-bold text-base my-2 lg:text-lg" :class="subtextColor">
@@ -41,7 +41,7 @@
         </a>
       </div>
       <div :class="`hidden md:${props.imageStatus2} md:mt-0 md:col-span-7 md:flex md:mx-auto lg:w-10/12`">
-        <nuxt-img format="webp" fit="cover" :src="props.imagePath" :alt="props.alt" class="rounded-lg shadow-lg my-auto h-46 w-80 lg:h-69 lg:w-120 xl:h-76 xl:w-132" />
+        <nuxt-img format="webp" :src="props.imagePath" :alt="props.alt" class="rounded-lg shadow-lg my-auto h-46 w-80 object-cover lg:h-69 lg:w-120 xl:h-76 xl:w-132" />
       </div>
     </div>
   </section>
