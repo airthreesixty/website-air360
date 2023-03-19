cf25a39d0d4b8871bf1d28f0c33057480aa50fa7
koki <koki.takahashi@scalefast.com>
2023-03-19 16:26:48 +0900

    Creat the primary button component

---
+++ components/ButtonPrimary.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="text-white bg-primary-600 text-base font-bold w-full mt-5 inline-block transition ease-in-out duration-300 hover:bg-primary-700 font-medium rounded-lg text-sm px-6 py-3 mr-2 mb-2 focus:outline-none">
+    <slot />
+  </button>
+</template>
--- components/content/SingleDescription.vue
+++ components/content/SingleDescription.vue
@@ -13,12 +13,12 @@
         <h3
           class="mb-4 mt-[22px] text-xl font-semibold text-black-600 text-left border-deco md:text-2xl lg:text-3xl dark:text-white"
         >
-        <ContentSlot :use="$slots.title" unwrap="p" />
+          <ContentSlot :use="$slots.title" unwrap="p" />
         </h3>
         <p
           class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-base lg:text-xl xl:leading-8 dark:text-gray-400"
         >
-        <ContentSlot :use="$slots.description" unwrap="p" />
+          <ContentSlot :use="$slots.description" unwrap="p" />
         </p>
         <a
           href="#"
