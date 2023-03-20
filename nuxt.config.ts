569d728746d3ac5c79113fa06689883468660220
koki <koki.takahashi@scalefast.com>
2023-03-20 09:29:57 +0900

    Switch the way of getting a sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', 'nuxt-windicss', '@nuxtjs/i18n', 'nuxt-font-loader', '@nuxtjs/algolia', '@nuxt/image-edge', '~/modules/sitemap', '@nuxtjs/robots'],
+  modules: ['@nuxt/content', 'nuxt-windicss', '@nuxtjs/i18n', 'nuxt-font-loader', '@nuxtjs/algolia', '@nuxt/image-edge', '@nuxtjs/robots'],
   nitro: {
     compressPublicAssets: true,
     prerender: {
