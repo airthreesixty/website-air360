74147774588e599c2396847b4f81740ea757943a
koki <koki.takahashi@scalefast.com>
2023-04-04 15:54:25 +0900

    change the hover action

--- components/ButtonPrimary.vue
+++ components/ButtonPrimary.vue
@@ -1,5 +1,6 @@
 <template>
-  <NuxtLink :to="$localePath(prop.slug)" class="text-white text-center bg-primary-600 text-base font-bold w-full px-6 py-3 inline-block transition ease-in-out duration-300 hover:bg-primary-700 rounded-lg focus:outline-none">
+  <!-- <NuxtLink :to="$localePath(prop.slug)" class="text-white text-center bg-primary-600 text-base font-bold w-full px-6 py-3 inline-block transition ease-in-out duration-300 hover:bg-primary-700 rounded-lg focus:outline-none"> -->
+  <NuxtLink :to="$localePath(prop.slug)" class="button">
     <slot />
   </NuxtLink>
 </template>
@@ -14,3 +15,74 @@ const prop = defineProps({
   },
 })
 </script>
+
+<style scoped>
+  /* svg {
+    stroke-dasharray: 150 480;
+    stroke-dashoffset: 150;
+    transition: 1s ease-in-out;
+  }
+
+  .btn {
+    --width: 180px;
+    --height: 60px;
+    position: relative;
+  }
+
+  .btn:hover svg {
+    stroke-dashoffset: -480;
+  } */
+
+  a.button {
+    display: block;
+    text-align: center;
+    vertical-align: middle;
+    text-decoration: none;
+    width: 100%;
+    margin: auto;
+    padding: 12px 24px;
+    color: #fff;
+    font-weight: bold;
+    background: #e74b91;
+    position: relative;
+    transition: 0.3s ease-in-out;
+    border-radius: 8px;
+}
+a.button:hover {
+    background: #fff;
+    color: #e74b91;
+    border-radius: 8px;
+}
+a.button:before, a.button:after {
+  box-sizing: inherit;
+  content: "";
+  position: absolute;
+  border: 2px solid transparent;
+  width: 0;
+  height: 0;
+  border-radius: 8px;
+}
+a.button:before {
+  top: 0;
+  left: 0;
+}
+a.button:after {
+  bottom: 0;
+  right: 0;
+}
+a.button:hover:before, a.button:hover:after {
+  width: 100%;
+  height: 100%;
+}
+a.button:hover:before {
+  border-top-color: #e74b91;
+  border-right-color: #e74b91;
+  transition: width 0.15s ease-out, height 0.15s ease-out 0.15s;
+}
+a.button:hover:after {
+  border-bottom-color: #e74b91;
+  border-left-color: #e74b91;
+  transition: border-color 0s ease-out 0.2s, width 0.15s ease-out 0.2s, height 0.15s ease-out 0.3s;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <ContentPage />
+  <div>
+    <!-- <div class="container bg-[#5CA4EA] h-screen w-100 h-100 absolute left-[50%] top-[50%] flex justify-center items-center">
+      <div class="w-45 h-15 absolute">
+        <button class="btn w-45 h-15 cursor-pointer bg-transparent border-1 border-[#91C9FF] border-solid outline-none transition ease-in-out duration-1000 hover:transition hover:ease-in-out hover:duration-1000 hover:bg-[#4F95DA]">
+          <svg
+            width="180px"
+            height="60px"
+            viewBox="0 0 180 60"
+            class="absolute left-0 top-0 fill-none stroke-[#fff]"
+          >
+            <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+            <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+          </svg>
+          <span class="text-white text-lg font-weight-100">HOVER ME</span>
+        </button>
+      </div>
+    </div> -->
+    <ContentPage />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -25,3 +43,21 @@ useSeoMeta({
   twitterCard: 'summary_large_image',
 })
 </script>
+
+<style scoped>
+  svg {
+    stroke-dasharray: 150 480;
+    stroke-dashoffset: 150;
+    transition: 1s ease-in-out;
+  }
+
+  .btn {
+    --width: 180px;
+    --height: 60px;
+    position: relative;
+  }
+
+  .btn:hover svg {
+    stroke-dashoffset: -480;
+  }
+</style>
