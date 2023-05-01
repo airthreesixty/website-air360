6ab879d0377c4e3cf6872b8e0ae8d0519658479e
koki <koki.takahashi@scalefast.com>
2023-05-01 17:30:33 +0900

    Changed the logo color as original

--- components/content/CompanyLogos.vue
+++ components/content/CompanyLogos.vue
@@ -21,32 +21,32 @@
 const logos = [
   {
     id: 1,
-    src: '/clubMedLogo.png',
+    src: '/logos/clubmed.svg',
     alt: 'Company Logo',
   },
   {
     id: 2,
-    src: 'aristocrazyLogo.png',
+    src: '/logos/aristocrazyLogo.png',
     alt: 'Company Logo',
   },
   {
     id: 3,
-    src: 'olympiqueLogo.png',
+    src: '/logos/olympique.webp',
     alt: 'Company Logo',
   },
   {
     id: 4,
-    src: 'maisonMargielaLogo.png',
+    src: '/logos/maisonMargielaLogo.png',
     alt: 'Company Logo',
   },
   {
     id: 5,
-    src: 'qoqaLogo.png',
+    src: '/logos/qoqa.svg',
     alt: 'Company Logo',
   },
   {
     id: 6,
-    src: 'suarezLogo.png',
+    src: '/logos/suarezLogo.png',
     alt: 'Company Logo',
   },
 ]
