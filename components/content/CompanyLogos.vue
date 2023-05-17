c78ac895c6c12f786ece89cb3e92c9f11319f1a9
Nicolas Hamelin <n.hamelin@ncit.nc>
2023-05-17 12:16:57 +1100

    Improve company logos

--- components/content/CompanyLogos.vue
+++ components/content/CompanyLogos.vue
@@ -1,18 +1,14 @@
 <template>
-  <section class="bg-white">
+  <section>
     <div class="mx-auto max-w-screen-[500px] md:max-w-screen-[560px] lg:max-w-screen-[800px] px-6 lg:px-8">
       <p class="text-center text-sm font-bold font-sans2 text-black-600 mb-8 tracking-widest">
         TRUSTED BY
       </p>
       <div
-        class="grid grid-cols-2 gap-y-14 gap-x-6 md:grid-cols-3"
+        class="grid grid-cols-2 gap-y-10 gap-x-6 md:grid-cols-3"
       >
-        <div v-for="logo in logos" :key="logo.id" class="flex justify-center items-center">
-          <img
-            class="h-6.5 object-contain w-full lg:h-7.5"
-            :src="logo.src"
-            :alt="logo.alt"
-          >
+        <div v-for="(logo, index) in logos" :key="index" class="flex justify-center items-center company-logos-list">
+          <component :is="logo.component" class="h-7.5 object-contain w-full lg:h-9.5" :style="`transform: scale(${logo.scale})`" />
         </div>
       </div>
     </div>
@@ -20,36 +16,43 @@
 </template>
 
 <script setup lang="ts">
-const logos = [
-  {
-    id: 1,
-    src: '/logos/ClubMed.svg',
-    alt: 'Company Logo',
-  },
-  {
-    id: 2,
-    src: '/logos/aristocrazy.svg',
-    alt: 'Company Logo',
-  },
-  {
-    id: 3,
-    src: '/logos/OlympiqueLyonnais.svg',
-    alt: 'Company Logo',
-  },
-  {
-    id: 4,
-    src: '/logos/MaisonMargiela.svg',
-    alt: 'Company Logo',
-  },
-  {
-    id: 5,
-    src: '/logos/Qoqa.svg',
-    alt: 'Company Logo',
-  },
-  {
-    id: 6,
-    src: '/suarezLogo.png',
-    alt: 'Company Logo',
-  },
-]
+const LogoBrandClubMed = resolveComponent('LogoBrandClubMed')
+const LogoBrandAristocrazy = resolveComponent('LogoBrandAristocrazy')
+const LogoBrandOlympiqueLyonnais = resolveComponent('LogoBrandOlympiqueLyonnais')
+const LogoBrandMaisonMargiela = resolveComponent('LogoBrandMaisonMargiela')
+const LogoBrandQoqa = resolveComponent('LogoBrandQoqa')
+const LogoBrandSuarez = resolveComponent('LogoBrandSuarez')
+
+const logos = [{
+  component: LogoBrandClubMed,
+  scale: .8,
+},
+{
+  component: LogoBrandAristocrazy,
+  scale: .85,
+},
+{
+  component: LogoBrandOlympiqueLyonnais,
+  scale: 1,
+},
+{
+  component: LogoBrandMaisonMargiela,
+  scale: 1,
+},
+{
+  component: LogoBrandQoqa,
+  scale: .75,
+},
+{
+  component: LogoBrandSuarez,
+  scale: .7,
+}]
 </script>
+
+<style lang="postcss">
+.company-logos-list {
+  path {
+    @apply !fill-gray-500;
+  }
+}
+</style>
