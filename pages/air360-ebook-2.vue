c0e4f1c53d0060f9af46477f342428e6c63c74aa
koki <koki.takahashi@scalefast.com>
2023-05-25 09:22:51 +0900

    Resize the image of books

--- pages/air360-ebook-2.vue
+++ pages/air360-ebook-2.vue
@@ -3,7 +3,7 @@
     <template #content>
       <h1>Why Google Analytics is not enough to grow your brand</h1>
       <h2>Getting the full picture of site performance</h2>
-      <nuxt-img src="/why-ga-is-not-enought.jpg" format="webp" loading="lazy" width="456" class="w-35 sm:w-57 float-right shadow ml-5" />
+      <nuxt-img src="/why-ga-is-not-enought.jpg" format="webp" loading="lazy" width="456" class="w-35 sm:w-65 float-right shadow ml-5" />
       <p>Next-generation user experience analytics tools are a necessary complement to Google Analytics.</p>
       <p>To get the best results from your GA setup, consider a tool that offers the customer insights that Google Analytics lacks. This eBook identifies eight areas where Google Analytics alone comes up short and explains how CRO tools bridge the gap with:</p>
       <ul>
