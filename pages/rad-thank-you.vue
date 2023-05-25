6dedeb8e40922ad9a32f602eafefadf39959ec3c
koki <koki.takahashi@scalefast.com>
2023-05-25 16:14:31 +0900

    Created rad-thank-you

---
+++ pages/rad-thank-you.vue
@@ -0,0 +1,17 @@
+<template>
+  <ThankYouPage>
+    <div class="flex flex-col justify-center items-center text-center">
+      <h1 class="page-thank-you__title">
+        <span class="inline-block">Woohoo!</span>
+        <span class="inline-block">Thank you for booking a meeting with us!</span>
+      </h1>
+      <MochiFlying class="mt-12 mb-10" />
+      <p class="page-thank-you__description">
+        We will be reaching out to you shortly to tailor our offer to your needs.
+      </p>
+      <p class="page-thank-you__description pt-5">
+        In the meantime grab a cup of coffee and find more inspiration with these blog posts.
+      </p>
+    </div>
+  </ThankYouPage>
+</template>
