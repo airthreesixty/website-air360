76fba4c1ca31e1bcc2e034c3e6a5af7a635ee79d
koki <koki.takahashi@scalefast.com>
2023-05-26 13:34:57 +0900

    Added the toggle currency

---
+++ components/Calculator/RoiCalculator.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container flex justify-center mx-auto">
+    <div class="py-20 max-w-[600px]">
+      <div class="">
+        <h1 class="text-black-600 font-bold text-2xl mb-6 md:text-3xl lg:text-4xl">
+          How much additional revenue can you generate thanks to Air360 in the next 6 months?
+        </h1>
+        <CalculatorToggleCurrency />
+        <div class="border-1 border-gray-200 rounded-lg shadow-md px-5 py-5 mt-12">
+          <h2 class="text-black-600 font-bold text-xl mb-5">
+            Calculate
+          </h2>
+          <CalculatorInput id="sessions" label="Online store's monthly sessions" type="text" class="mb-4" />
+          <CalculatorInput id="conversion-rate" label="Current conversion rate(%)" type="text" class="mb-4" />
+          <CalculatorInput id="average-order-value" label="Average order value" type="text" />
+          <div class="flex justify-center">
+            <button
+              class="text-white bg-primary-600 my-8 text-base font-bold inline-block transition ease-in-out duration-300 hover:bg-primary-700 rounded-lg w-full py-3 focus:outline-none"
+            >
+              Calculate
+            </button>
+          </div>
+          <p>Blablabla</p>
+          <hr>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const runtimeConfig = useRuntimeConfig()
+const route = useRoute()
+const { t } = useI18n()
+useSeoMeta({
+  title: t('roi-calc.title'),
+  description: t('roi-calc.description'),
+  ogDescription: t('roi-calc.description'),
+  ogTitle: t('roi-calc.title'),
+  ogUrl: `${runtimeConfig.public.baseUrl}${route.fullPath}`,
+})
+</script>
---
+++ components/Calculator/ToggleCurrency.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-around">
+    <nuxt-link v-for="tag in tagList" :key="tag" :to="$localePath(`/roi-calculator-${tag.toLowerCase()}`)" class="tag__link">
+      {{ tag }}
+    </nuxt-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { $localePath } = useNuxtApp()
+const tagList = ['USD', 'EUR', 'Yen']
+</script>
+
+<style scoped lang="postcss">
+.tag__link {
+  @apply py-3 px-4 mt-5 md:px-5 inline-block text-sm lg:text-lg bg-blue-100 text-blue-400 font-semibold rounded mx-1 md:mx-2 transition-colors;
+
+  &.router-link-exact-active,
+  &:hover {
+    @apply text-white bg-blue-400;
+  }
+}
+</style>
