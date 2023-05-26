39a70ae91ae95f2b1a60c3e80d790881d448e17d
koki <koki.takahashi@scalefast.com>
2023-05-26 14:30:26 +0900

    Translation

--- components/Calculator/RoiCalculator.vue
+++ components/Calculator/RoiCalculator.vue
@@ -3,7 +3,7 @@
     <div class="py-20 max-w-[600px]">
       <div class="">
         <h1 class="text-black-600 font-bold text-2xl mb-6 md:text-3xl lg:text-4xl">
-          How much additional revenue can you generate thanks to Air360 in the next 6 months?
+          {{ $t('roi-calc.header') }}
         </h1>
         <CalculatorToggleCurrency />
         <div class="border-1 border-gray-200 rounded-lg shadow-md px-5 py-5 mt-12">
@@ -11,15 +11,15 @@
             Calculate
           </h2>
           <div class="flex flex-col gap-4">
-            <CalculatorInputComponent id="sessions" label="Online store's monthly sessions" type="text" />
-            <CalculatorInputComponent id="conversion-rate" label="Current conversion rate(%)" type="text" />
-            <CalculatorInputComponent id="average-order-value" :label="`Average order value(${currency})`" type="text" />
+            <CalculatorInputComponent id="sessions" :label="$t('roi-calc.label1')" type="text" />
+            <CalculatorInputComponent id="conversion-rate" :label="$t('roi-calc.label2')" type="text" />
+            <CalculatorInputComponent id="average-order-value" :label="$t('roi-calc.label3') + `(${currency})`" type="text" />
           </div>
           <div class="flex justify-center">
             <button
               class="text-white bg-primary-600 my-8 text-base font-bold inline-block transition ease-in-out duration-300 hover:bg-primary-700 rounded-lg w-full py-3 focus:outline-none"
             >
-              Calculate
+              {{ $t('roi-calc.calculate') }}
             </button>
           </div>
           <p>Blablabla</p>
