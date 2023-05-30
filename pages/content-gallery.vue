dc555c1e8312a2814521c0093d252d11a87f9634
koki <koki.takahashi@scalefast.com>
2023-05-30 15:45:05 +0900

    Redisigned the content gallery

--- pages/content-gallery.vue
+++ pages/content-gallery.vue
@@ -1,29 +1,24 @@
 <template>
-  <div class="container mt-15 mx-auto">
-    <div class="mb-5">
-      <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl text-center mb-10 lg:mb-15">
-        Content Gallery
-      </h1>
-      <nuxt-link :to="$localePath('/ga4-migration')">
-        <div class="border shadow-md rounded-lg mx-auto px-3 lg:px-8 py-5 md:(grid grid-cols-2 gap-10)">
+  <div class="">
+    <div class="container mx-auto">
+      <nuxt-link :to="$localePath('/ga4-migration')" class="card transform hover:scale-95">
+        <div class="mx-auto px-3 lg:px-8 py-15 md:(grid grid-cols-2 gap-10)">
           <div class="flex flex-col justify-center">
-            <h2 class="border-deco-gallery text-2xl md:text-3xl font-bold hover:underline">
+            <h2 class="border-deco-gallery text-2xl md:text-3xl md:leading-10 font-bold hover:underline">
               The eCommerce experience analytics solution that will help you simplify your Google Analytics 4 migration
             </h2>
-            <p class="text-gray-500 text-base lg:text-lg mb-4">
-              If you are here, it is because you are feeling under pressure with the sunset of Universal Analytics in July 2023.
-              This Google Analytics 4 migration might be overwhelming, but Air360 can help.
-              Watch our quick video that explains how you can solve your challenges in this migration.
-            </p>
           </div>
           <div class="md:self-center">
-            <nuxt-img src="/en/ga4-migration.webp" class="w-full object-cover rounded-lg" />
+            <nuxt-img src="/en/ga4-migration.webp" class="w-full object-cover rounded-lg duration-300" />
           </div>
         </div>
       </nuxt-link>
     </div>
 
-    <section class="bg-white">
+    <section class="bg-gray-50 container mx-auto py-10">
+      <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl text-center mt-8 lg:mt-10">
+        Content Gallery
+      </h1>
       <div class="py-8  mx-auto lg:py-16">
         <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
           <GalleryCard title="Why you need a UX analytics tool & how to get it" image-src="/air360-ebook-3-cover.webp" slug="/air360-ebook-3" />
@@ -71,6 +66,11 @@ useSeoMeta({
 
 <style lang="postcss">
 .border-deco-gallery {
-  @apply after:(content-[''] block w-25 h-[5px] bg-[#E74B91] rounded-[7px] static my-4);
+  @apply after:(content-[''] block w-25 h-[5px] bg-[#E74B91] rounded-[7px] static my-5);
+}
+
+.card:hover img {
+  transform: scale(0.95);
+  transition-duration: 300ms;
 }
 </style>
