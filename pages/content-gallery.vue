76eb4b1361c55b8eb2d4334b782e408cc769f922
koki <koki.takahashi@scalefast.com>
2023-05-30 15:49:49 +0900

    Fixed the container place

--- pages/content-gallery.vue
+++ pages/content-gallery.vue
@@ -15,21 +15,22 @@
       </nuxt-link>
     </div>
 
-    <section class="bg-gray-50 container mx-auto py-10">
-      <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl text-center mt-8 lg:mt-10">
-        Content Gallery
-      </h1>
-      <div class="py-8  mx-auto lg:py-16">
-        <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-          <GalleryCard title="Why you need a UX analytics tool & how to get it" image-src="/air360-ebook-3-cover.webp" slug="/air360-ebook-3" />
-          <GalleryCard
-            title="Why Google Analytics is not enough to grow your brand"
-            image-src="/why-ga-is-not-enought.jpg"
-            slug="/air360-ebook-2"
-          />
-          <GalleryCard title="Five conversion blockers and how to fix them" image-src="/five-conversion-blockers.webp" slug="/air360-ebook-1" />
-
-          <GalleryCard title="Conversion rate mysteries" image-src="/air360-cr-mysteries.jpg" slug="/mystery-case" />
+    <section class="bg-gray-50 mx-auto py-10">
+      <div class="container mx-auto">
+        <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl text-center mt-8 lg:mt-10">
+          Content Gallery
+        </h1>
+        <div class="py-8  mx-auto lg:py-16">
+          <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+            <GalleryCard title="Why you need a UX analytics tool & how to get it" image-src="/air360-ebook-3-cover.webp" slug="/air360-ebook-3" />
+            <GalleryCard
+              title="Why Google Analytics is not enough to grow your brand"
+              image-src="/why-ga-is-not-enought.jpg"
+              slug="/air360-ebook-2"
+            />
+            <GalleryCard title="Five conversion blockers and how to fix them" image-src="/five-conversion-blockers.webp" slug="/air360-ebook-1" />
+            <GalleryCard title="Conversion rate mysteries" image-src="/air360-cr-mysteries.jpg" slug="/mystery-case" />
+          </div>
         </div>
       </div>
     </section>
