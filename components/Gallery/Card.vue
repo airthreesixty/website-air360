dafb50ad07b19c98e6aa1bd07e67765ce3f56b9f
koki <koki.takahashi@scalefast.com>
2023-05-30 11:17:47 +0900

    Created content-gallery

--- components/Gallery/Card.vue
+++ components/Gallery/Card.vue
@@ -1,30 +1,31 @@
 <template>
-  <nuxt-link>
+  <nuxt-link :to="$localePath(slug)">
     <article
       class="relative p-4 bg-white h-full rounded-lg border border-gray-200 shadow-md"
     >
       <nuxt-img
         format="webp"
-        class="mb-5 rounded-lg h-50 w-full object-cover"
+        class="mb-5 rounded-lg w-full object-cover"
         :src="imageSrc"
       />
       <h2
-        class="mt-2 mb-2 text-xl font-bold tracking-tight text-black-600 dark:text-white"
+        class="mt-2 mb-2 text-2xl font-bold tracking-tight text-black-600 dark:text-white"
       >
         {{ title }}
       </h2>
-      <p class="text-gray-500">
+      <!-- <p class="text-gray-500">
         {{ description }}
-      </p>
+      </p> -->
     </article>
   </nuxt-link>
 </template>
 
 <script setup lang="ts">
+const { $localePath } = useNuxtApp()
 interface Props {
   title: string,
-  description: string,
-  imageSrc: string
+  imageSrc: string,
+  slug: string
 }
 
 defineProps<Props>()
--- pages/content-gallery.vue
+++ pages/content-gallery.vue
@@ -1,34 +1,48 @@
 <template>
-  <div class="container my-15 mx-auto">
-    <div class="">
-      <h1 class="font-bold text-3xl md:text-4xl text-center mb-10">
+  <div class="container mt-15 mx-auto">
+    <div class="mb-5">
+      <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl text-center mb-10">
         Content Gallery
       </h1>
-      <div class="border shadow-md rounded-lg max-w-[1000px] mx-auto px-3 py-5 md:(grid grid-cols-2)">
+      <div class="border shadow-md rounded-lg mx-auto px-3 lg:px-4 py-5 md:(grid grid-cols-2 gap-10)">
         <div class="flex flex-col justify-center">
-          <h2 class="border-deco-gallery text-xl md:text-2xl font-bold">
-            Five conversion blockers and how to fix them
+          <h2 class="border-deco-gallery text-2xl md:text-3xl font-bold">
+            The eCommerce experience analytics solution that will help you simplify your Google Analytics 4 migration
           </h2>
           <p class="text-gray-500 text-base lg:text-lg mb-4">
-            Businesses both large and small benefit from detailed and actionable CX insights. Yet many companies are reluctant to invest, and many stakeholders may need convincing.
+            If you are here, it is because you are feeling under pressure with the sunset of Universal Analytics in July 2023.
+            This Google Analytics 4 migration might be overwhelming, but Air360 can help.
+            Watch our quick video that explains how you can solve your challenges in this migration.
           </p>
-          <p class="text-primary-600 mb-2">
+          <nuxt-link class="text-primary-600 mb-4" :to="$localePath('/ga4-migration')">
             <span class="font-bold">More</span><fa-icon class="fa-sm pl-1" :icon="['fas', 'chevron-right']" />
-          </p>
+          </nuxt-link>
         </div>
-        <nuxt-img src="/air360-ebook-3-cover.webp" class="w-full h-60 sm:h-65 md:h-75 object-cover rounded-lg" />
+        <nuxt-img src="/en/ga4-migration.webp" class="w-full object-cover rounded-lg" />
       </div>
     </div>
 
     <section class="bg-white">
       <div class="py-8  mx-auto lg:py-16">
         <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-          <GalleryCard title="Hello" description="Blabla" image-src="/air360-ebook-3-cover.webp" />
+          <GalleryCard title="Why you need a UX analytics tool & how to get it" image-src="/air360-ebook-3-cover.webp" slug="/air360-ebook-3" />
+          <GalleryCard
+            title="Why Google Analytics is not enough to grow your brand"
+            image-src="/why-ga-is-not-enought.jpg"
+            slug="/air360-ebook-2"
+          />
+          <GalleryCard title="Five conversion blockers and how to fix them" image-src="/five-conversion-blockers.webp" slug="/air360-ebook-1" />
+
+          <GalleryCard title="Conversion rate mysteries" image-src="/air360-cr-mysteries.jpg" slug="/mystery-case" />
         </div>
       </div>
     </section>
   </div>
 </template>
+
+<script setup>
+const { $localePath } = useNuxtApp()
+</script>
 
 <style lang="postcss">
 .border-deco-gallery {
