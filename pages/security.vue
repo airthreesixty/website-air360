d6bfae935b6cb92d9ce266e61a089eda0b68f4e3
koki <koki.takahashi@scalefast.com>
2023-06-05 15:25:56 +0900

    Prepared meta tag template

--- pages/security.vue
+++ pages/security.vue
@@ -1,3 +1,15 @@
 <template>
   <ContentDoc />
 </template>
+
+<script setup lang="ts">
+// useSeoMeta({
+//   title,
+//   description,
+//   ogDescription: description,
+//   ogTitle: title,
+//   ogImage: `${runtimeConfig.public.baseUrl}/air360-cr-mysteries.jpg`,
+//   twitterCard: 'summary_large_image',
+//   ogUrl: `${runtimeConfig.public.baseUrl}${route.fullPath}`,
+// })
+</script>
