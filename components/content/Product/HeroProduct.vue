f522eb3e8cda3b6969b2f8640f8c3643157e8f93
koki <koki.takahashi@scalefast.com>
2023-06-09 11:53:36 +0900

    Created product hero

--- components/content/Product/HeroProduct.vue
+++ components/content/Product/HeroProduct.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>jhjh</h1>
+  <div class="container pt-10 md:pb-10 lg:pt-14">
+    <div class="md:(grid grid-cols-2)">
+      <div class="text-center md:text-left">
+        <span class="font-bold text-primary-600 text-base"><ContentSlot :use="$slots.subtext" unwrap="p" /></span>
+        <h1 class="title1 my-5 font-bold text-black-600">
+          <ContentSlot :use="$slots.title" unwrap="p" />
+        </h1>
+        <p class="text-gray-500 lg:text-lg xl:text-xl xl:leading-8">
+          <ContentSlot :use="$slots.description" unwrap="p" />
+        </p>
+        <div class="flex justify-center md:justify-start">
+          <div class="w-52 my-5">
+            <ButtonPrimary theme="primary">
+              Request a demo
+            </ButtonPrimary>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center">
+        <nuxt-img format="webp" src="/en/heroProduct.png" class="" loading="lazy" sizes="xl:1390px lg:1034px md:754px sm:1184px xs:277px" />
+      </div>
+    </div>
+  </div>
 </template>
--- content/en/product/website-analysis.md
+++ content/en/product/website-analysis.md
@@ -1,2 +1,9 @@
 ::hero-product
+#subtext
+COLLECT, DEFINE, ANALYZE & ACTIVATE
+#title
+Get insights fast and turn them into actions
+#description
+Air360 brings data analysts and business profiles together on the same mission.
+Increase conversion, unlock missed revenue, and turn frustrated users into loyal customers.
 ::
