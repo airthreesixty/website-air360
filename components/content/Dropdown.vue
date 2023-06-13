0760464e0c1a2729820486a50e6bd255d13686b1
koki <koki.takahashi@scalefast.com>
2023-06-13 15:08:56 +0900

    Adjust the style of dropdowns

--- components/content/Dropdown.vue
+++ components/content/Dropdown.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="mt-5">
     <button :aria-expanded="isOpen" data-toggle="collapse" class="w-full border-b border-gray-200" @click="onOpen">
-      <div class="text-black-600 font-bold flex justify-between items-center pb-3">
+      <div class="text-black-600 font-bold lg:text-lg flex justify-between items-center pb-3">
         <slot name="title" />
         <span><fa-icon :class="{'rotate-180': isOpen}" class="text-2xl ml-1 transform transition-all text-primary-600" :icon="['far', 'angle-down']" /></span>
       </div>
