402e49b3afa0eb7c636c172b695b128252821d28
koki <koki.takahashi@scalefast.com>
2023-06-14 17:37:17 +0900

    Added the link to the product menu

--- components/Dropdowns/ProductDropdown.vue
+++ components/Dropdowns/ProductDropdown.vue
@@ -6,10 +6,16 @@
           Features
         </div>
         <div class="text-sm text-black-600 mt-4">
-          <span><fa-icon class="text-lg text-primary-600 w-5 mr-2" :icon="['far', 'laptop']" /></span>Web analysis
+          <span><fa-icon class="text-lg text-primary-600 w-5 mr-2" :icon="['far', 'laptop']" /></span>
+          <NuxtLink :to="localePath('/product/website-analysis')">
+            Website analysis
+          </NuxtLink>
         </div>
         <div class="text-sm text-black-600 mt-4">
-          <span><fa-icon class="text-lg text-orange-600 w-5 mr-2" :icon="['far', 'mobile']" /></span>App analysis
+          <span><fa-icon class="text-lg text-orange-600 w-5 mr-2" :icon="['far', 'mobile']" /></span>
+          <NuxtLink :to="localePath('/product/app-analysis')">
+            App analysis
+          </NuxtLink>
         </div>
       </div>
       <div>
@@ -17,9 +23,16 @@
           Other
         </div>
         <div class="text-sm text-black-600 mt-4">
-          <span><fa-icon class="text-lg text-blue-400 mr-2" :icon="['far', 'file-contract']" /></span>  Security & Compliance
+          <span><fa-icon class="text-lg text-blue-400 mr-2" :icon="['far', 'file-contract']" /></span>
+          <NuxtLink :to="localePath('/security-compliance')">
+            Security & Compliance
+          </NuxtLink>
         </div>
       </div>
     </div>
   </nav>
 </template>
+
+<script setup lang="ts">
+const localePath = useLocalePath()
+</script>
