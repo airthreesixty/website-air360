756d10e339b22374bd879f410ccb686c979d190e
koki <koki.takahashi@scalefast.com>
2023-07-12 14:11:40 +0900

    Added the seo image

--- pages/product/capabilities/events-segments.vue
+++ pages/product/capabilities/events-segments.vue
@@ -13,5 +13,7 @@ useSeoMeta({
   ogDescription: t('events-segments.content'),
   ogTitle: t('events-segments.title'),
   ogUrl: `${runtimeConfig.public.baseUrl}${route.fullPath}`,
+  ogImage: `${runtimeConfig.public.baseUrl}/en/events-segments/hero.png`,
+  twitterCard: 'summary_large_image',
 })
 </script>
