e66c2e977e5e64a55cfcb4fb6175c0985cfcbc1b
koki <koki.takahashi@scalefast.com>
2023-07-14 08:59:59 +0900

    Wording on data integrations and data collection

--- plugins/js-sdk.client.js
+++ plugins/js-sdk.client.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 export default defineNuxtPlugin((nuxtApp) => {
-  if (nuxtApp.$config.public.platform === 'prod') {
+  // if (nuxtApp.$config.public.platform === 'prod') {
     // @ts-ignore
     window.Air360 = window.Air360 || [], Air360.init = function (e, t) { window.Air360.appid = e, window.Air360.config = t = t || {}; var a = document.createElement("script"); a.type = "text/javascript", a.async = !0, a.src = "https://cdn.air360tracker.net/air360.min.js"; var n = document.getElementsByTagName("script")[0]; n.parentNode.insertBefore(a, n); for (var o = function (e) { return function () { Air360.push([e].concat(Array.prototype.slice.call(arguments, 0))) } }, p = ["addEventProperties", "clearEventProperties", "identify", "removeEventProperty", "setUserProperties", "track", "trackError", "trackPurchase", "optInUserTracking", "optOutUserTracking"], c = 0; c < p.length; c++)Air360[p[c]] = o(p[c]) };
     Air360.init("izxdj2ihcqxarhox");
-  }
+  // }
 })
