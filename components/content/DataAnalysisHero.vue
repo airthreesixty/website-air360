1a7ec08190ca46217e8edcabfd0b0f70825891fb
koki <koki.takahashi@scalefast.com>
2023-07-13 12:10:59 +0900

    Completed the data-analysis page without images

--- components/content/DataAnalysisHero.vue
+++ components/content/DataAnalysisHero.vue
@@ -5,18 +5,21 @@
       <h1 class="title1 my-5 font-bold text-black-600">
         <ContentSlot :use="$slots.title" unwrap="p" />
       </h1>
-      <div class="text-gray-500 max-w-[650px] mx-auto lg:text-lg xl:text-xl xl:leading-8">
+      <div class="text-gray-500 max-w-[650px] mx-auto mb-2 md:mb-6 lg:text-lg xl:text-xl xl:leading-8">
         <ContentSlot :use="$slots.description" />
       </div>
-      <p class="animation-text animate__animated animate__bounceInRight">
-        Why is my checkout abandonment so high?
+      <p class="py-2 lg:py-5 animation-text animate__animated animate__bounceInRight">
+        <span class="highlight-container" style="--color: #e74b91; --delay: 0.7s;"><span class="highlight text-2xl md:text-3xl lg:text-4xl xl:text-5xl">Why is my checkout abandonment so high?</span></span>
       </p>
-      <p class="animation-text animate__animated animate__bounceInLeft delay-time1">
-        What segments convert better and why?
+      <p class="py-2 lg:py-5 animation-text animate__animated animate__bounceInLeft delay-time1">
+        <span class="highlight-container" style="--color: #FA734C; --delay: 2.7s;"><span class="highlight text-2xl md:text-3xl lg:text-4xl xl:text-5xl">What segments convert better and why?</span></span>
       </p>
-      <p class="animation-text animate__animated animate__bounceInRight delay-time2">
-        Why acquisition traffic is not converting?
+      <p class="py-2 lg:py-5 animation-text animate__animated animate__bounceInRight delay-time2">
+        <span class="highlight-container" style="--color: #3C8BFF; --delay: 4.7s;"><span class="highlight text-2xl md:text-3xl lg:text-4xl xl:text-5xl">Why acquisition traffic is not converting?</span></span>
       </p>
+      <h2 class="title2 mt-5 -mb-5 md:(mt-10 -mb-8) xl:mt-15 animation-text animate__animated animate__bounceInDown delay-time3">
+        Answer your questions with Air360
+      </h2>
     </div>
   </div>
 </template>
@@ -27,18 +30,51 @@ import 'animate.css'
 
 <style scoped>
 .animation-text {
-  font-size: 30px;
   color: #232E4A;
-  margin: 10px 0;
-  font-weight: bold;
+  background: linear-gradient(transparent 60%,  var(--color));
+  /* #fce041 */
 }
 .delay-time1 {
-  animation-delay: 1.5s;
+  animation-delay: 2s;
 }
 .delay-time2 {
-  animation-delay: 3s;
+  animation-delay: 4s;
 }
 .delay-time3 {
-  animation-delay: 4.5s;
+  animation-delay: 6s;
+}
+
+.highlight-container, .highlight {
+  position: relative;
+}
+
+.highlight-container {
+  display: inline-block;
+}
+.highlight-container:before {
+  content: " ";
+  display: block;
+  height: 90%;
+  width: 0%;
+  margin-left: -3px;
+  margin-right: -3px;
+  position: absolute;
+  background: var(--color);
+  transform: rotate(2deg);
+  top: -1px;
+  left: -1px;
+  border-radius: 20% 25% 20% 24%;
+  padding: 10px 3px 3px 10px;
+  animation: highlight 1s ease-in-out 1 forwards;
+  animation-delay: var(--delay);
+}
+
+@keyframes highlight {
+  0% {
+    width: 0%;
+  }
+  100% {
+    width: 100%;
+  }
 }
 </style>
--- content/en/product/capabilities/data-analysis.md
+++ content/en/product/capabilities/data-analysis.md
@@ -6,3 +6,42 @@ Answer unsolved questions fast
 #description
 Follow user footprints and fingerprints, and quickly discover the obstacles that make your audience leave your website without converting 
 ::
+
+::single-description{imagePath="/en/data-collection/cookies.png" :is-shadow="false" :container="true"}
+#subtext
+WHAT
+#title
+Understand User Trends 
+#description
+See what is happening on your eCommerce in a glance with Dashboards & Reports that will display descriptive metrics and trends of user behavior. 
+::
+
+::single-description{imagePath="/en/data-collection/noSample.png" imageStatus="block" imageStatus2="hidden" themeColor="#FA734C" :is-shadow="false" :container="true"}
+#subtext
+HOW
+#title
+Discover patterns and obstacles
+#description
+Understand how your users like to buy and where they are getting stuck during their  purchase process. Our Funnels and User Journeys will show you what paths they take during their journey in your eCommerce.
+::
+
+::single-description{imagePath="/en/data-collection/cookies.png" :is-shadow="false" :container="true"}
+#subtext
+WHY
+#title
+Get to the root cause
+#description
+Define hypothesis and validate them through our Sessions Replays. Have quick access to relevant replays with through filters to prescribe good recommendations for website optimization.
+::
+
+::single-description{imagePath="/en/data-collection/noSample.png" imageStatus="block" imageStatus2="hidden" themeColor="#FA734C" :is-shadow="false" :container="true"}
+#subtext
+WHO
+#title
+Quick wins to enhance your user experience
+#description
+Prioritize improvements based on the impact  they have on your business and target segments that did not convert  to give them a second change.
+::
+
+::cta-new
+::
