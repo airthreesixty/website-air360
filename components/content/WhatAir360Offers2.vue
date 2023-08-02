dab26dc6461c936ec4a3caad0814c19429675a31
koki <koki.takahashi@scalefast.com>
2023-08-02 15:46:46 +0900

    Test fixing image issue by turining off clientonly

--- components/content/WhatAir360Offers2.vue
+++ components/content/WhatAir360Offers2.vue
@@ -79,6 +79,6 @@ const posterSrc2 = computed(() => {
 })
 
 const image = computed(() => {
-  return locale.value === 'en' ? '/en/product/analyze.png' : '/en/product/analyze2.png'
+  return locale.value === 'en' ? '/en/product/analyze.png' : '/analyze.png'
 })
 </script>
