a44a0a56d7d38fa9f0706d7d6a15a29b7ebbd541
koki <koki.takahashi@scalefast.com>
2023-08-07 10:03:01 +0900

    Removed the old video's javascript

--- components/content/WhatIsAir360.vue
+++ components/content/WhatIsAir360.vue
@@ -12,6 +12,7 @@
       <div class="my-12">
         <div class="mx-auto md:w-9/12" style="position: relative; padding-bottom: 56.25%; height: 0;">
           <iframe
+            v-if="showIframe"
             src="https://www.loom.com/embed/ec06bf3738f64c8e960b2c0eccaf89b0?sid=cb91bf0c-e504-4098-91d4-c7afbbb919dc?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true"
             frameborder="0"
             webkitallowfullscreen
@@ -36,10 +37,6 @@ useIntersectionObserver(
   target,
   ([{ isIntersecting }]) => {
     if (!showIframe.value && isIntersecting) {
-      const s = document.createElement('script')
-      s.setAttribute('src', 'https://fast.wistia.net/assets/external/E-v1.js')
-      document.body.appendChild(s)
-
       showIframe.value = true
     }
   },
