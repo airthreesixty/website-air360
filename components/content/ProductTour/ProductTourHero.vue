eab94197229decddf9b3c3e6fc7608991af6232a
koki <koki.takahashi@scalefast.com>
2023-08-14 11:16:05 +0900

    Created the product tour page

---
+++ components/content/ProductTour/ProductTourHero.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div class="container">
+      <h1 class="title2 max-w-[1000px] mx-auto text-center mt-10">
+        <ContentSlot :use="$slots.title" unwrap="p" />
+      </h1>
+      <p class="textp text-center md:max-w-[600px] lg:max-w-[800px] mx-auto my-5">
+        <ContentSlot :use="$slots.description" unwrap="p" />
+      </p>
+      <div class="md:max-w-[700px] mx-auto">
+        <iframe src="https://www.videoask.com/f13f7jxmz" allow="camera *; microphone *; autoplay *; encrypted-media *; fullscreen *; display-capture *;" width="100%" height="600px" style="border: none; border-radius: 24px" />
+      </div>
+      <p class="textp text-center md:max-w-[600px] lg:max-w-[800px] mx-auto my-6">
+        <ContentSlot :use="$slots.description2" unwrap="p" />
+      </p>
+      <!-- <div class="pt-10 mb-15">
+        <CompanyLogos />
+      </div> -->
+      <SliderCompanies />
+    </div>
+  </section>
+</template>
---
+++ content/en/product-tour.md
@@ -0,0 +1,8 @@
+::product-tour-hero
+#title
+Uncover hidden behaviors and <div class="md:inline-block">make smarter decisions.</div>
+#description
+Air360 eCommerce experience analytics platform helps you spot all opportunities to improve & optimize your user journeys, get the recommendations you need to increase conversion & prioritize decisions to boost growth.
+#description2
+Watch our product tour and learn how to collect all behavioral and purchase data without any tagging or mapping, define the interactions and segments needed to visualize in simple steps, get insights analyzing your users with easy-to-use tools, and activate your segments through third-party automation.
+::
