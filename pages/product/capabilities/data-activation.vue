78728f12a550594a8a17248f841649ebccd2ed14
koki <koki.takahashi@scalefast.com>
2023-08-16 17:37:07 +0900

    added the meta info on Data activation

--- pages/product/capabilities/data-activation.vue
+++ pages/product/capabilities/data-activation.vue
@@ -1,3 +1,19 @@
 <template>
   <ContentDoc />
 </template>
+
+<script setup>
+const { t } = useI18n()
+const route = useRoute()
+const runtimeConfig = useRuntimeConfig()
+
+useSeoMeta({
+  title: t('data-analysis.title'),
+  description: t('data-analysis.content'),
+  ogDescription: t('data-analysis.content'),
+  ogTitle: t('data-analysis.title'),
+  ogUrl: `${runtimeConfig.public.baseUrl}${route.fullPath}`,
+  ogImage: `${runtimeConfig.public.baseUrl}/en/data-activation/hero.png`,
+  twitterCard: 'summary_large_image',
+})
+</script>
