f5cc195207d1b18bf9011cca543ea614d7495e58
Koki-T3 <koki.takahashi@scalefast.com>
2023-09-08 11:11:09 +0900

    Added the animation to mochiCart

--- components/content/Promotion/ChecklistHero.vue
+++ components/content/Promotion/ChecklistHero.vue
@@ -1,8 +1,11 @@
 <template>
-  <section class="bg-white py-10 lg:py-14">
+  <section class="bg-white py-10 lg:py-14 overflow-hidden">
     <div class="text-center container md:(grid grid-cols-2 items-center justify-around gap-3)">
-      <div class="max-w-[500px] mx-auto">
+      <div class="relative max-w-[500px] mx-auto">
         <nuxt-img src="/promotion/checklist.png" class="mb-8" />
+        <div class="absolute -left-55 -bottom-10 lg:bottom-0 mochi-cart-wrapper">
+          <MochiCart class="w-50 h-50 lg:(w-60 h-60) mochi-cart" />
+        </div>
       </div>
       <div class="rounded-lg max-w-[1050px] mx-auto p-5 lg:p-10">
         <div>
@@ -26,6 +29,7 @@
 
 <script setup>
 import { useScriptTag } from '@vueuse/core'
+import { gsap } from 'gsap'
 
 useScriptTag(
   '//js-eu1.hsforms.net/forms/embed/v2.js',
@@ -42,10 +46,39 @@ useScriptTag(
       },
     })
   })
+
+onMounted(() => {
+  setTimeout(() => {
+    let largeDevice = 1
+
+    if (window.innerWidth >= 1024) {
+      largeDevice = 1.25
+    }
+    gsap.to('.mochi-cart-wrapper', {
+      ease: 'power2.inOut',
+      keyframes: [
+        { x: 0, y: 0, rotation: 0, duration: 0.23 },
+        { x: 30 * largeDevice, y: 0, rotation: -5, duration: 0.23 },
+        { x: 60 * largeDevice, y: -1, rotation: 0, duration: 0.23 },
+        { x: 90 * largeDevice, y: -1, rotation: 5, duration: 0.23 },
+        { x: 120 * largeDevice, y: 0, rotation: -4, duration: 0.23 },
+        { x: 150 * largeDevice, y: 1, rotation: 0, duration: 0.23 },
+        { x: 180 * largeDevice, y: 0, rotation: 4, duration: 0.23 },
+        { x: 210 * largeDevice, y: 0, rotation: -3, duration: 0.23 },
+        { x: 240 * largeDevice, y: 1, rotation: 0, duration: 0.23 },
+        { x: 270 * largeDevice, y: 0, rotation: 3, duration: 0.23 },
+        { x: 300 * largeDevice, y: 0, rotation: -2, duration: 0.23 },
+        { x: 330 * largeDevice, y: 1, rotation: 0, duration: 0.23 },
+        { x: 360 * largeDevice, y: 0, rotation: 2, duration: 0.23 },
+        { x: 390 * largeDevice, y: 0, rotation: 0, duration: 0.32 },
+      ],
+    })
+  }, 1000)
+})
 </script>
 
-<style>
-input.hs-button.primary.large {
-  width: 100%;
+<style scoped>
+.mochi-cart {
+  transform: scale(-1, 1);
 }
 </style>
