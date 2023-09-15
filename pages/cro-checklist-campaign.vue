42c30e86c678517b2ae28103bb1573b6219ef7e2
Koki-T3 <koki.takahashi@scalefast.com>
2023-09-15 19:30:39 +0900

    Fixed ogImage

--- pages/cro-checklist-campaign.vue
+++ pages/cro-checklist-campaign.vue
@@ -7,9 +7,17 @@
 
 <script setup>
 import { ModalsContainer } from 'vue-final-modal'
+
 const { t } = useI18n()
 const route = useRoute()
 const runtimeConfig = useRuntimeConfig()
+
+const imgSrc = computed(() => {
+  if (locale.value === 'en') {
+    return '/en/promotion/checklist.png'
+  }
+  return '/promotion/checklist.png'
+})
 
 useSeoMeta({
   title: t('promotion-lp.title'),
@@ -17,7 +25,7 @@ useSeoMeta({
   ogDescription: t('promotion-lp.content'),
   ogTitle: t('promotion-lp.title'),
   ogUrl: `${runtimeConfig.public.baseUrl}${route.fullPath}`,
-  ogImage: `${runtimeConfig.public.baseUrl}/promotion/checklist.png`,
+  ogImage: `${runtimeConfig.public.baseUrl}${imgSrc.value}`,
   twitterCard: 'summary_large_image',
 })
 </script>
