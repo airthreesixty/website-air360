0f84b78433cec2eb7ed15e58e58f7dcae5e78fb2
Koki-T3 <koki.takahashi@scalefast.com>
2023-10-02 13:40:53 +0900

    Uploaded the doc for downloading

--- pages/cro-roadmap-campaign-thank-you.vue
+++ pages/cro-roadmap-campaign-thank-you.vue
@@ -2,14 +2,14 @@
   <ThankYouPage>
     <div class="flex flex-col justify-center items-center text-center">
       <h1 class="page-thank-you__title">
-        {{ $t('cro-checklist-thank-you.title') }}
+        {{ $t('cro-roadmap-thank-you.title') }}
       </h1>
       <MochiFlying class="mt-12 mb-10" />
       <p class="page-thank-you__description">
-        {{ $t('cro-checklist-thank-you.desc') }}
+        {{ $t('cro-roadmap-thank-you.desc') }}
       </p>
-      <a class="page-thank-you__button" href="/promotion/cro-checklist.pdf" download="/promotion/cro-checklist.pdf" target="_blank">
-        {{ $t('cro-checklist-thank-you.cta') }}
+      <a class="page-thank-you__button" :href="href" :download="href" target="_blank">
+        {{ $t('cro-roadmap-thank-you.cta') }}
       </a>
     </div>
   </ThankYouPage>
@@ -25,9 +25,16 @@ const description = t('cro-checklist-thank-you.desc')
 
 const imgSrc = computed(() => {
   if (locale.value === 'en') {
-    return '/en/promotion/checklist.png'
+    return '/en/promotion/roadmapHeroPoster.png'
   }
-  return '/promotion/checklist.png'
+  return '/en/promotion/roadmapHeroPoster.png'
+})
+
+const href = computed(() => {
+  if (locale.value === 'en') {
+    return '/en/promotion/CRORoadmapTemplate.xlsx'
+  }
+  return '/en/promotion/CRORoadmapTemplate.xlsx'
 })
 
 useSeoMeta({
