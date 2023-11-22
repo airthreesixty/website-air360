f9476c9836ac25023e6a0dca846c2a6d3d01d213
Koki-T3 <koki.takahashi@scalefast.com>
2023-11-22 14:04:41 +0900

    Switch the video

--- components/content/Homepage/WhatAir360Offers2.vue
+++ components/content/Homepage/WhatAir360Offers2.vue
@@ -74,32 +74,32 @@
 const { locale } = useI18n()
 
 const videoSrc1 = computed(() => {
-  return locale.value === 'en' ? '/en/videos/1.mp4' : '/en/videos/1.mp4'
+  return locale.value === 'en' ? '/en/videos/1.mp4' : '/videos/collect.mp4'
 })
 
 const posterSrc1 = computed(() => {
-  return locale.value === 'en' ? '/en/posters/1.png' : '/en/posters/1.png'
+  return locale.value === 'en' ? '/en/posters/1.png' : '/posters/collect.png'
 })
 
 const videoSrc2 = computed(() => {
-  return locale.value === 'en' ? '/en/videos/2.mp4' : '/en/videos/2.mp4'
+  return locale.value === 'en' ? '/en/videos/2.mp4' : '/videos/define.mp4'
 })
 const posterSrc2 = computed(() => {
-  return locale.value === 'en' ? '/en/posters/2.png' : '/en/posters/2.png'
+  return locale.value === 'en' ? '/en/posters/2.png' : '/posters/define.png'
 })
 
 const videoSrc3 = computed(() => {
-  return locale.value === 'en' ? '/en/videos/3.mp4' : '/en/videos/3.mp4'
+  return locale.value === 'en' ? '/en/videos/3.mp4' : '/videos/analyze.mp4'
 })
 
 const posterSrc3 = computed(() => {
-  return locale.value === 'en' ? '/en/posters/3.png' : '/en/posters/3.png'
+  return locale.value === 'en' ? '/en/posters/3.png' : '/posters/analyze.png'
 })
 const videoSrc4 = computed(() => {
-  return locale.value === 'en' ? '/en/videos/4.mp4' : '/en/videos/4.mp4'
+  return locale.value === 'en' ? '/en/videos/4.mp4' : '/videos/activate.mp4'
 })
 
 const posterSrc4 = computed(() => {
-  return locale.value === 'en' ? '/en/posters/4.png' : '/en/posters/4.png'
+  return locale.value === 'en' ? '/en/posters/4.png' : '/posters/activate.png'
 })
 </script>
