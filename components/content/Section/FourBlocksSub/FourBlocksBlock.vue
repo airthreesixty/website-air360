b8bc8ae4b1af35174f41e581ffaafc7e8f94ea0c
Koki-T3 <koki.takahashi@scalefast.com>
2023-12-13 14:21:04 +0900

    adjust font color

--- components/content/Section/FourBlocksSub/FourBlocksBlock.vue
+++ components/content/Section/FourBlocksSub/FourBlocksBlock.vue
@@ -3,10 +3,10 @@
     <div class="flex justify-center items-center mb-4 w-10 h-10 rounded bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
       <fa-icon class="text-2xl lg:text-3xl text-primary-600" :icon="['far', icon]" />
     </div>
-    <h3 class="mb-2 text-xl font-bold dark:text-white">
+    <h3 class="mb-2 text-xl font-bold text-black-600">
       <ContentSlot :use="$slots.title" unwrap="p" />
     </h3>
-    <p class="font-light text-gray-500 dark:text-gray-400">
+    <p class="text-gray-500">
       <ContentSlot :use="$slots.desc" unwrap="p" />
     </p>
   </div>
