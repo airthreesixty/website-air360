51e15e4ec62cd84116bd19bd7641db9f6d561d94
Koki-T3 <koki.takahashi@scalefast.com>
2023-12-14 13:15:55 +0900

    Lazy loading

--- components/content/Homepage/TeamsSub/TeamsBlock.vue
+++ components/content/Homepage/TeamsSub/TeamsBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center grid-card">
-    <nuxt-img format="webp" class="mx-auto w-60 h-60 rounded-lg object-cover" :src="src" alt="Avatar" />
+    <nuxt-img format="webp" loading="lazy" class="mx-auto w-60 h-60 rounded-lg object-cover" :src="src" alt="Avatar" />
     <div class="-mb-5 text-2xl text-white font-bold">
       <ContentSlot :use="$slots.role" unwrap="p" />
     </div>
