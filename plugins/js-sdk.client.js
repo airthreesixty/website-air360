483696f20d204dca4c5be3fc2a3dcc1afcc2a9a2
Nicolas Hamelin <n.hamelin@ncit.nc>
2024-01-03 09:12:30 +1100

    Update Air360 snippet

--- plugins/js-sdk.client.js
+++ plugins/js-sdk.client.js
@@ -2,7 +2,7 @@
 export default defineNuxtPlugin((nuxtApp) => {
   if (nuxtApp.$config.public.platform === 'prod') {
     // @ts-ignore
-    window.Air360 = window.Air360 || [], Air360.init = function (e, t) { window.Air360.appid = e, window.Air360.config = t = t || {}; var a = document.createElement("script"); a.type = "text/javascript", a.async = !0, a.src = "https://cdn.air360tracker.net/air360.min.js"; var n = document.getElementsByTagName("script")[0]; n.parentNode.insertBefore(a, n); for (var o = function (e) { return function () { Air360.push([e].concat(Array.prototype.slice.call(arguments, 0))) } }, p = ["addEventProperties", "clearEventProperties", "identify", "removeEventProperty", "setUserProperties", "track", "trackError", "trackPurchase", "optInUserTracking", "optOutUserTracking"], c = 0; c < p.length; c++)Air360[p[c]] = o(p[c]) };
+    window.Air360 = window.Air360||[],Air360.init=Air360.init||function(e){window.Air360.appid=e;var a=document.createElement("script");a.type="module",a.async=!0,a.src="https://cdn.air360tracker.net/v2/air360.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){Air360.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","clearEventProperties","identify","removeEventProperty","setUserProperties","track","trackError","trackPurchase","optInUserTracking","optOutUserTracking"],c=0;c<p.length;c++)Air360[p[c]]=o(p[c])};
     Air360.init("izxdj2ihcqxarhox");
   }
 })
