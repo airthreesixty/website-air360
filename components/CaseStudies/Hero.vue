c748520872f1f3cfe11eff421e40513dfb37011f
Koki-T3 <koki@air360.io>
2024-05-10 11:24:02 +0900

    Create case study components

---
+++ components/CaseStudies/Hero.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container pt-10 pb-6 md:pb-10 lg:pt-14">
+    <div class="md:(grid grid-cols-2)">
+      <div class="text-center md:text-left">
+        <span class="font-bold text-primary-600 text-base uppercase">New case studies</span>
+        <h1 class="title1 my-5 font-bold text-black-600">
+          The psychology behind highly effective landing pages
+        </h1>
+        <div class="text-gray-500 prose lg:text-lg xl:text-xl xl:leading-8">
+          <ContentSlot :use="$slots.description" />
+        </div>
+        <div class="flex justify-center md:justify-start">
+          <div class="flex flex-col mt-5 md:flex-row md:gap-4">
+            <div class="w-52 md:mb-0">
+              <ButtonPrimary theme="primary" slug="javascript:void(0)">
+                {{ $t("request-demo.title") }}
+              </ButtonPrimary>
+            </div>
+            <p v-if="app" class="mt-2 md:mt-0 text-black-600 flex items-center justify-center">
+              <ContentSlot :use="$slots.subDescription" />
+              <span class="ml-1"><img src="/iosIcon.svg" class="w-10" alt="IOS icon"></span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center">
+        <!-- <nuxt-img format="webp" :src="src" loading="lazy" sizes="xl:1390px lg:1034px md:754px sm:1184px xs:277px" /> -->
+        image
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  app?: boolean
+  src: string
+}
+
+withDefaults(defineProps<Props>(), {
+  app: false,
+})
+
+const { locale } = useI18n()
+</script>
