a5db6688a661cd8e30fe4f99eec2a684e3f11544
Koki-T3 <reunion.fy0317@gmail.com>
2024-12-21 09:06:14 +0900

    Fix3

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,23 @@
-import createNextIntlPlugin from 'next-intl/plugin';
-import createMDX from '@next/mdx';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
-import remarkFrontmatter from 'remark-frontmatter';
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import remarkGfm from 'remark-gfm';
-import { contentRedirect } from './redirects-list.mjs';
-import { withContentlayer } from 'next-contentlayer';
-import withBundleAnalyzer from '@next/bundle-analyzer';
+import createNextIntlPlugin from "next-intl/plugin";
+import createMDX from "@next/mdx";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import remarkFrontmatter from "remark-frontmatter";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import remarkGfm from "remark-gfm";
+import { contentRedirect } from "./redirects-list.mjs";
+import { withContentlayer } from "next-contentlayer";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true'
+  enabled: process.env.ANALYZE === "true",
 });
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  output: "export",
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   reactStrictMode: true,
   swcMinify: true,
   // output: 'export',
@@ -25,21 +26,23 @@ const nextConfig = {
   },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
 
     config.module.rules.push(
       // Reapply the existing rule, but only for svg imports ending in ?url
       {
         ...fileLoaderRule,
         test: /\.svg$/i,
-        resourceQuery: /url/ // *.svg?url
+        resourceQuery: /url/, // *.svg?url
       },
       // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
         resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ['@svgr/webpack']
+        use: ["@svgr/webpack"],
       }
     );
 
@@ -47,7 +50,7 @@ const nextConfig = {
     fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
-  }
+  },
 };
 
 const withMDX = createMDX({
@@ -58,20 +61,22 @@ const withMDX = createMDX({
       [
         rehypeAutolinkHeadings,
         {
-          behavior: 'wrap',
+          behavior: "wrap",
           properties: (node) => {
-            const title = node?.children?.[0]?.value || '';
+            const title = node?.children?.[0]?.value || "";
             return {
               title,
               ariaLabel: `Anchor to ${title}`,
-              className: ['anchor-link']
+              className: ["anchor-link"],
             };
           },
-          test: (node) => node.tagName !== 'h1'
-        }
-      ]
-    ]
-  }
+          test: (node) => node.tagName !== "h1",
+        },
+      ],
+    ],
+  },
 });
 
-export default bundleAnalyzer(withNextIntl(withContentlayer(withMDX(nextConfig))));
+export default bundleAnalyzer(
+  withNextIntl(withContentlayer(withMDX(nextConfig)))
+);
