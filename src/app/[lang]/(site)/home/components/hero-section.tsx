13036197581c1db79aa26eeac0b27071b9307610
Koki-T3 <reunion.fy0317@gmail.com>
2025-01-09 08:25:49 +0900

    Adjust main title and subtitle size

--- src/app/[lang]/(site)/home/components/hero-section.tsx
+++ src/app/[lang]/(site)/home/components/hero-section.tsx
@@ -15,11 +15,11 @@ const HeroSection: React.FC<HeroSectionProps> = ({ lang }) => {
   return (
     <div className="flex flex-col items-center">
       <div className="flex justify-center md:mb-5 lg:mb-10">
-        <div className="relative inline-block">
+        <div className="relative inline-block max-w-[950px]">
           <h1
             className={cn(
-              "mb-2 font-black font-really text-[42px] tracking-wide text-gradient select-none md:text-7xl lg:text-8xl xl:text-9xl text-center lg:mb-4",
-              lang === "ja" && "text-[38px] md:text-6xl lg:text-7xl xl:text-8xl"
+              "mb-2 font-black font-really text-[42px] tracking-wide text-gradient select-none md:text-7xl lg:text-8xl text-center lg:mb-4",
+              lang === "ja" && "text-[38px] md:text-6xl lg:text-7xl"
             )}
           >
             <span className="block leading-20">{t("hero.title.top")}</span>
@@ -27,7 +27,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({ lang }) => {
               <span className="leading-20">{t("hero.title.bottom")}</span>
             )}
           </h1>
-          <p className="text-gray-450 text-lg font-really text-center md:text-2xl lg:text-3xl xl:text-4xl">
+          <p className="text-gray-450 text-lg font-really text-center md:text-2xl lg:text-3xl">
             {t("hero.subtitle")}
           </p>
           <div className="flex justify-center mt-4 lg:mt-6">
