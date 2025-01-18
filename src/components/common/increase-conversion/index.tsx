0e12198a79721d807ef608dbbfa2d49b7351f8d6
Koki-T3 <reunion.fy0317@gmail.com>
2025-01-18 09:45:37 +0900

    Fix the bug the mochi now showing on mobile device

--- src/components/common/increase-conversion/index.tsx
+++ src/components/common/increase-conversion/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import MochiFlying from "/public/vectors/mochi-flying.svg";
+import MochiFlyingReverse from "/public/vectors/mochi-flying-reverse.svg";
 import ButtonCta from "@/components/ui/button-cta";
 import { Link } from "@/i18n/routing";
 import { useTranslations } from "next-intl";
@@ -11,7 +11,7 @@ const IncreaseConversion = () => {
   return (
     <div className="m-auto text-center md:px-12 lg:px-20">
       <div className="relative mx-auto flex h-20 w-20 sm:h-24 sm:w-24 items-center justify-center">
-        <MochiFlying className="relative m-auto" />
+        <MochiFlyingReverse className="relative m-auto transform -scale-x-100 -rotate-6" />
       </div>
       <div className="relative">
         <h2 className="mb-4 font-really text-4xl font-bold text-black-600 mx-auto select-none sm:text-6xl lg:text-7xl">
