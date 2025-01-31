c594e84a87d74a618d3d1b909129f832f018222b
Koki-T3 <reunion.fy0317@gmail.com>
2025-02-01 08:51:41 +0900

    Adjust grid

--- src/app/[lang]/(withoutHeader)/request-demo/components/form/index.tsx
+++ src/app/[lang]/(withoutHeader)/request-demo/components/form/index.tsx
@@ -108,7 +108,7 @@ export function RequestDemoForm({ lang }: { lang: string }) {
               </FormItem>
             )}
           />
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
             <FormField
               control={form.control}
               name="email"
